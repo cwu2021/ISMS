ba69a11e43966353fdc6bad9df9eecd53d1467b0
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-01-09 09:58:23 +0800

    sync via ssh and cron

--- backup/sync-data.sh
+++ backup/sync-data.sh
@@ -6,5 +6,7 @@ do
 echo "`date` sync started..." >> sync.log
 # estimate total sync time.
 rsync -avz /folder1 /folder2/
+# or sync remotely via ssh, cron at midnight, morning, aftrnoon, and such.
+rsync -av user1@host1:/data/folder/ /local/folder/
 done
 echo "`date` sync ended..." >> sync.log
