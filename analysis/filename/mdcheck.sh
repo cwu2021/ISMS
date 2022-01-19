2ab355b9f10b114debe5ae65013713a32a5b698c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-01-19 09:24:51 +0800

    check MDaemon disabled accounts.

---
+++ analysis/filename/mdcheck.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+cat Accounts.csv | while read LINE
+do
+# grep -a for chinese characters
+# http://lausai360.blogspot.com/2017/04/grep-binary-file-standard-input-matches.html
+# https://www.everything-mdaemon.com/mdaemon/how-to-delete-all-disabled-accounts
+echo $LINE | grep -a '0,0,C' | cut -d ',' -f1
+done
