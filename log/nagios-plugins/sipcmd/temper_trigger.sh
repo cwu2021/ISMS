e375ba76ec5af202ad496ad7180cc9fd826d2ea0
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-01-23 01:57:49 +0800

    sip call out when triggered.

---
+++ log/nagios-plugins/sipcmd/temper_trigger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+usernum=
+userpass=
+sipserver=
+sipport=
+
+declare -i temp=`/your/temper/code.sh`
+#if [ $temp -gt 15 ];then
+# modified due to high humidity after typhoon, Aug 10, 2021.
+if [ $temp -gt 25 ];then
+sipcmd -u $usernum -c $userpass -P sip -w $sipserver:$sipport -x "c5551;ws3000;vsiptest.wav;h"
+fi
