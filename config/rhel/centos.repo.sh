4b364c104b9914f49b52eea18974eb3ca8c76fcf
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-02-22 09:44:45 +0800

    repo of CodeReady Linux Builder, etc.

---
+++ config/rhel/centos.repo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Introducing CodeReady Linux Builder
+# https://developers.redhat.com/blog/2018/11/15/introducing-codeready-linux-builder
+# The AppStream repository
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/package_manifest/appstream-repository
+less /etc/yum.repos.d/centos.repo
+# How to activate your no-cost Red Hat Enterprise Linux subscription
+# https://developers.redhat.com/blog/2021/02/10/how-to-activate-your-no-cost-red-hat-enterprise-linux-subscription#step_2__download_no_cost_rhel
