b2071fda917d95ac2ca5ab2503e2793c975ac720
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-02 13:57:08 +0800

    LCP keepalive issue.

---
+++ workflow/infra/openbsd-pppoe.sh
@@ -0,0 +1,5 @@
+#!/bin/ksh
+# LCP keepalive timeout every 350 sec after ISP upgrade. Not seen on proprietary or linux based routers.
+tail /var/log/messages
+# more tuning to pppoe may be required.
+man pppoe
