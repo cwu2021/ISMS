bee0db742825023f86c25ec20112ed78686b1c7b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-08 10:43:34 +0800

    email isForwarding users check.

--- analysis/filename/mdcheck.sh
+++ analysis/filename/mdcheck.sh
@@ -5,4 +5,6 @@ do
 # http://lausai360.blogspot.com/2017/04/grep-binary-file-standard-input-matches.html
 # https://www.everything-mdaemon.com/mdaemon/how-to-delete-all-disabled-accounts
 echo $LINE | grep -a '0,0,C' | cut -d ',' -f1
+# isForwarding.
+echo $LINE | grep -a '0,1,Y' | cut -d ',' -f1
 done
