a1fe101831fb6d339799edfed0bedc900927462f
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-10 00:14:17 +0800

    CPU thermal status

---
+++ workflow/infra/CPU_thermal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat /sys/class/thermal/thermal_zone*/temp
+# https://askubuntu.com/questions/1110943/what-do-the-different-thermal-zones-actually-correspond-to
+# https://www.twblogs.net/a/5c7bf98abd9eee339918bbe9
