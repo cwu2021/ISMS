6dbcbe970e18dadf782ad330c8109f50a1f2df41
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-15 00:14:19 +0800

    sensors on linux including eth check.

--- workflow/infra/CPU_thermal.sh
+++ workflow/infra/CPU_thermal.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cat /sys/class/thermal/thermal_zone*/temp
 # https://askubuntu.com/questions/1110943/what-do-the-different-thermal-zones-actually-correspond-to
 # https://www.twblogs.net/a/5c7bf98abd9eee339918bbe9
+cat /sys/class/thermal/thermal_zone*/temp
+# How to Check Temperature on Linux
+# https://phoenixnap.com/kb/linux-cpu-temp#:~:text=Check CPU Temperature Using Lm-Sensors,-Lm-sensors is&text=Wait for the lm-sensors,all sensors in the machine.
+cat /sys/class/net/eth*/operstate
+# https://unix.stackexchange.com/questions/121523/check-if-interface-eth0-is-up-configured
