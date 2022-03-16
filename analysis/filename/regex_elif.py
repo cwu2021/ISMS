4755623552795dff52869a1059f355ea004e40af
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-17 00:15:36 +0800

    add more regex as condition with elif

---
+++ analysis/filename/regex_elif.py
@@ -0,0 +1,6 @@
+# add more regex as condition with elif
+# https://www.footmark.com.tw/news/programming-language/python/python-if-elif-else/
+if False:
+    print('if True')
+elif True:
+    print('elif True')
