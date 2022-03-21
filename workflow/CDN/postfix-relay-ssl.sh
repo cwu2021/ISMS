ea5e83a2f9abe9a51927382567319b2feec14983
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-21 10:17:51 +0800

    postfix relay and SSL.

---
+++ workflow/CDN/postfix-relay-ssl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# postfix relay with iptables input block. opensmtpd !for gmail relay host smtps:// . changing external gw ip useless to unblock gmail.
+# google workspace same mx as gmail...
+# https://wiki.centos.org/HowTos/postfix_sasl#Configuring_SSL.2FTLS_in_postfix
