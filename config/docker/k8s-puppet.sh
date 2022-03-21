4a156b2fe83a8d280bca82075d5266fe8c976d8c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-21 09:57:54 +0800

    k8s via puppt or snapd.

---
+++ config/docker/k8s-puppet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# microk8s.io with 4G/20G, also, microstack with 8G/100G.
+# https://microk8s.io/docs/getting-started
+# canonical oficially via snapd.
+# or k8s via puppet module
+sudo apt install puppet
+sudo puppet help module
+# https://forge.puppet.com/modules/puppetlabs/kubernetes
+# https://zhekunhu.xyz/ps4-kubernetes.html
+# also massive nrpe via puppet
