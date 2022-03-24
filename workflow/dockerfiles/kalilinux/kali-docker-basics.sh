0c44ca1a3da7edaa945e98d4b146248d22de782b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-24 09:50:00 +0800

    some basic tests via kalilinux in docker.

---
+++ workflow/dockerfiles/kalilinux/kali-docker-basics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt update
+apt -y install kali-linux-headless
+apt install ping
+apt install iputils-ping
+nmap -p 80 -A $target_ip
+cd /usr/share/nmap/
+cd scripts/
+ls *http*
+wpscan
+nslookup $target_url
+wpscan --url $target_url --enumerate p
+wpscan --url http://heavenkid.ntdtv.com.tw --enumerate p --random-user-agent
+wpscan --url $target_ip --enumerate p --random-user-agent -exit-api-token $your_token
+nmap -v -A $target_ip
