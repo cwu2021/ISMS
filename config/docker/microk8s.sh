58fa1c5ba4b28894c2b768126ba32512cd263add
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-03-30 10:24:35 +0800

    k8s on a single ubuntu host.

---
+++ config/docker/microk8s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# kubectl for Docker Users
+# https://kubernetes.io/docs/reference/kubectl/docker-cli-to-kubectl/
+# https://microk8s.io/docs/getting-started
+sudo snap install microk8s --classic --channel=1.21
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+su - $USER
+vi ~/.bash_aliases) like this:
+# alias kubectl='microk8s kubectl'
+kubectl create deployment nginx --image=nginx
+kubectl get pods
+# https://kubernetes.io/docs/tasks/debug-application-cluster/get-shell-running-container/
+kubectl exec --stdin --tty nginx-6799fc88d8-kkxf6 -- /bin/bash
+kubectl get pods
