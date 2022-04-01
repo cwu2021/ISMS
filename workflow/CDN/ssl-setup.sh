7f6249678fc243edaa8316dcf86b776c3ae139b3
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-04-01 11:38:32 +0800

    update and optimize SSL pem.

--- workflow/CDN/ssl-setup.sh
+++ workflow/CDN/ssl-setup.sh
@@ -3,3 +3,9 @@
 # https://blog.cloudthat.com/cloudfront-custom-ssl-certificate/
 echo | openssl s_client -showcerts -servername gnupg.org -connect gnupg.org:443 2>/dev/null | openssl x509 -inform pem -noout -text
 # https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools
+# no need to include TrustedRoot.cer but DigiCertCA.cer to optimize bandwidth usage and page latency. Browsers have root CA but no intermediate ones.
+cat mysite.cer > mysite.pem
+cat DigiCertCA.cer >> mysite.pen
+# https://unix.stackexchange.com/questions/32001/what-is-m-and-how-do-i-get-rid-of-it
+dos2unix mysite.pem
+# check validation via https://www.ssllabs.com/ssltest/index.html
