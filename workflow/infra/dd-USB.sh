98a384b04edaa76585b818754f4718190dc9cffb
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-04-03 11:31:27 +0800

    dd create pve installation usb.

--- workflow/infra/dd-USB.sh
+++ workflow/infra/dd-USB.sh
@@ -2,3 +2,6 @@
 # this method works for creating bootable USB from img (OpenBSD, FreeDOS) and iso (Ubuntu).
 # https://www.openbsd.org/faq/faq4.html
 dd if=install70.img of=/dev/rsd6c bs=1M
+# https://www.proxmox.com/en/downloads/category/iso-images-pve
+# https://pve.proxmox.com/wiki/Prepare_Installation_Media
+dd bs=1M conv=fdatasync if=./proxmox-ve_*.iso of=/dev/XYZ
