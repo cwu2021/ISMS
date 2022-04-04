fa48a62f31d8f9eba9ca4e8c718629187f93dcde
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-04-04 10:21:05 +0800

    Java versions of licensing concern.

---
+++ devops/java_versions.sh
@@ -0,0 +1,15 @@
+#!/bin/ksh
+# latest version of Oracle Java without license fee is 8u202. AdoptOpenJDK can be used for later versions.
+doas pkg_add -v jdk
+# Update candidates: quirks-4.53 -> quirks-4.53
+# quirks-4.53 signed on 2021-09-28T09:08:28Z
+# Ambiguous: choose package for jdk
+# a     0: <None>
+#       1: jdk-1.8.0.302.b08.1p0v0
+#       2: jdk-11.0.12.7.1v0
+#       3: jdk-16.0.2.7.1v0
+# jdk-1.8.0.302.b08.1p0v0: ok
+# New and changed readme(s):
+#       /usr/local/share/doc/pkg-readmes/jdk-1.8.0
+# --- +jdk-1.8.0.302.b08.1p0v0 -------------------
+# You may wish to add /usr/local/jdk-1.8.0/man to /etc/man.conf
