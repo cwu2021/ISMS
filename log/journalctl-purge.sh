e2584ad74ee81d4d8477f5a4c26bb51d0049e20d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-04-14 00:19:32 +0800

    Delete journal logs

---
+++ log/journalctl-purge.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Delete journal logs older than X days: sudo journalctl --vacuum-time=2days.
+#Delete log files until the disk space taken falls below the specified size:
+sudo journalctl --vacuum-size=100M.
+# https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjP1ImM6o73AhVmyosBHTBbA4QQFnoECAgQAw&url=https%3A%2F%2Fubuntuhandbook.org%2Findex.php%2F2020%2F12%2Fclear-systemd-journal-logs-ubuntu%2F&usg=AOvVaw3oBlAx0JLt2HLbXg3psGfr
