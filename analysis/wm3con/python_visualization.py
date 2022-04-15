77a04f60a50d7891019f80205d5992d081d9822a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-04-16 00:14:19 +0800

    data visualization with python

---
+++ analysis/wm3con/python_visualization.py
@@ -0,0 +1,3 @@
+# http://books.gotop.com.tw/download/ACD021300
+# http://dlcenter.gotop.com.tw/SampleFiles/ACD021300/download/Python_Visualization_sample.zip
+# with matplotlib, seaborn, pandas, numpy, scipy.
