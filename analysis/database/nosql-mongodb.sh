54b1eaf269232eb92f66e562a79981fc85e3aaf3
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-05-01 00:17:52 +0800

    nosql with mongodb

---
+++ analysis/database/nosql-mongodb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://ubuntu.com//blog/what-is-mongodb
+# The most famous feature of MongoDB is its flexible data store because of its JSON-like document format.
+# Another feature of MongoDB is that it offers an efficient way to search data with text, geospatial, or time-series dimensions. In addition, MongoDB includes features to analyse data, including support for multiple concurrent queries, indexing, and aggregation. Recent versions of MongoDB also include support for distributed, multi-document, multi-collection, multi-database, and multi-shard transactions with a high guarantee of data integrity.
+apt search mongodb
