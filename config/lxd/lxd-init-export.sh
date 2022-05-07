b1df428c7d55cfb4859714c395fe76578b73bd1d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-05-08 00:16:13 +0800

    lxd init and export.

---
+++ config/lxd/lxd-init-export.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+lxc list
+# If this is your first time running LXD on this machine, you should also run: lxd init
+# To start your first instance, try: lxc launch ubuntu:20.04
+lxc export
+# https://www.team-bob.org/lxd_backup_and_migrate/
