0e540c10b6a86656b1938a033506c9daa891ee2b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-05-28 00:14:18 +0800

    nslookup TXT for SPF record.

--- analysis/geoip/nslookup.sh
+++ analysis/geoip/nslookup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # nslookup.io as the web app.
 # Find DNS records for a domain name using this online tool.
-nslookup
+nslookup -type=txt your.domain 168.95.1.1
+# Gmail requires valid SPF for accepting delivery, e.g. v=spf1 ip4:your.smtp/24 a mx ~all
+# https://code.yidas.com/nslookup/
