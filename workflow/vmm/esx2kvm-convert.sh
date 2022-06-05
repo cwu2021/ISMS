1330d21fab10b9ac097586adfea105a475db4001
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-05 09:54:12 +0800

    vmdk migration procedure.

--- workflow/vmm/esx2kvm-convert.sh
+++ workflow/vmm/esx2kvm-convert.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # https://docs.openstack.org/image-guide/convert-images.html
 qemu-img convert -f vmdk -O qcow2 image.vmdk image.qcow2
+# - converted from vmdk to qcow2. raw better on zfs.
+# - replace hdd in dir storage. writeback on zfs.
+# - all NIC tied on internal vmbr.
+# - move hdd to zfs storage as an option.
+# https://forum.proxmox.com/threads/create-vm-from-existing-qcow2-image.33331/
+# https://forum.proxmox.com/threads/new-virutal-machine-will-not-start.44889/
