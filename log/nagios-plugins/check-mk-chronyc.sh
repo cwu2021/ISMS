77241a83f04855879197215e5ded1cf5f5c1e8a6
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-14 12:25:33 +0800

    chronyc speedup check patch.

---
+++ log/nagios-plugins/check-mk-chronyc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://forum.checkmk.com/t/check-mk-english-ntp-time-check-returns-randomly-crit-state/12221
+# no DNS lookup: chronyc -n tracking
+mv /usr/bin/chronyc /usr/bin/chronyc.bak
+cat > /usr/bin/chronyc << EOF
+/usr/bin/chronyc.bak -n $1
+EOF
