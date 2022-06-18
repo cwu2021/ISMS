f9c896e475410a364f33e1c3037efed60c8d1969
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-18 12:13:35 +0800

    chromecast via cmd.

---
+++ workflow/ffmpeg/chromecast-npm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://github.com/erkstruwe/chromecast-cli
+# npm related packages required. about 162MB.
+sudo dnf install npm
+sudo pm install -g chromecast-cli
+chromecast --host 192.168.1.100 status
+chromecast --host 192.168.1.100 play http://192.168.1.1/media/song.mp3
+sudo npm uninstall -g chromecast-cli
+sudo dnf remove npm nodejs nodejs-libs openssl nodejs-docs nodejs-full-i18n
