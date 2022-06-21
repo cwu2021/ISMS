797a8147d2355cb2246e8a69330b6cd8026dd444
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-21 18:49:08 +0800

    music control on Windows 10 via cmd.

---
+++ workflow/ffmpeg/music-win10.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Window+R for cmd on Windows 10 https://www.computerhope.com/issues/ch001041.htm
+start music.mp3
+REM Groove keyboard shortcuts F7,F8,F9 https://support.microsoft.com/en-us/windows/keyboard-shortcuts-in-apps-139014e7-177b-d1f3-eb2e-7298b2599a34#bkmk_groove
+REM how can I use taskkill to close out groove music
+Taskkill /IM music.UI.exe /F
