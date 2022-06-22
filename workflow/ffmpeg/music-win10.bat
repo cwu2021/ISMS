318a71dd2579e8eb906fd4e6b2fc38f600f596a4
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-22 22:24:15 +0800

    vlc for more supported formats

--- workflow/ffmpeg/music-win10.bat
+++ workflow/ffmpeg/music-win10.bat
@@ -4,3 +4,6 @@ start music.mp3
 REM Groove keyboard shortcuts F7,F8,F9 https://support.microsoft.com/en-us/windows/keyboard-shortcuts-in-apps-139014e7-177b-d1f3-eb2e-7298b2599a34#bkmk_groove
 REM how can I use taskkill to close out groove music
 Taskkill /IM music.UI.exe /F
+REM vlc for more supported formats: ctrl-up/down,f/esc https://wiki.videolan.org/QtHotkeys/
+c:\program files\videolan\vlc\vlc.exe --fullscreen video.rmvb
+taskkill /im vlc.exe /f
