057ff61d04fe7ebc58b0976333851f6ad1b4068d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-25 12:50:31 +0800

    backup all db locally.

--- backup/ssh-mysqldump.sh
+++ backup/ssh-mysqldump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/40024280/mysqldump-via-ssh-to-local-computer
+mysqldump --all-databases > backup/alldb-`date +%Y%m%d`.sql
 ssh root@ipaddress "mysqldump -u dbuser -p dbname | gzip -9" > dblocal.sql.gz 
