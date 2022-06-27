fc4daeaa02c0980de40d2307ac6fa4142de890d4
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-06-27 21:42:45 +0800

    set keepcache=1 in yum.conf

--- config/rhel/yum-cache-rpm.sh
+++ config/rhel/yum-cache-rpm.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 # yum cache as local rpm repo.
+# Where are the rpm files after installation using yum? - Super User
+# https://superuser.com/questions/171302/where-are-the-rpm-files-after-installation-using-yum
+ls /var/cache/yum
+# in /etc/yum.conf , set keepcache=1 .
