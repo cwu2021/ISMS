c601772eb64cfa67eb5c407e7eaf752ae48331a0
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-09 10:30:10 +0800

    SSH to machine A via B in one command.

---
+++ ACL/ssh-nc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# How do I SSH to machine A via B in one command?
+cat >> .ssh/config << EPF
+Host unibroker          # Machine B definition (the broker)
+Hostname 12.34.45.56    # Change this IP address to the address of the broker
+User myusername         # Change this default user accordingly 
+                        # (`user@unibroker` can overwrite it)
+
+Host internalmachine    # Machine A definition (the target host)
+ProxyCommand ssh -q unibroker nc hostname.or.IP.address.internal.machine 22
+IdentityFile your_key_on_local
+EOF
