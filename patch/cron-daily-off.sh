e040d05009c4b8db18c401c2536d68f3f735b6ee
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-14 09:09:10 +0800

    timers stop and disabled.

--- patch/cron-daily-off.sh
+++ patch/cron-daily-off.sh
@@ -4,4 +4,8 @@
 sudo systemctl list-timers
 sudo systemctl stop apt-daily-upgrade.timer
 sudo systemctl stop apt-daily.timer
+# http://www.ruanyifeng.com/blog/2018/03/systemd-timer.html
+sudo systemctl list-unit-files --type timer
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl disable apt-daily.timer
 sudo systemctl daemon-reload
