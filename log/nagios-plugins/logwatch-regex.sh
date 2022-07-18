89ea99b910a1ebc086f052649f965a0fa1240d49
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-18 16:50:11 +0800

    certutil in KDC err.

--- log/nagios-plugins/logwatch-regex.sh
+++ log/nagios-plugins/logwatch-regex.sh
@@ -1,4 +1,4 @@
 # https://blog.gtwang.org/perl/perl-course-notes-6/
 # 不指定比對目標，預設為 $_
 # Windows event log of KDC error for smartcard.
-print "It matches\n" if /KDC.*KDC.*KDC.*certtuil.*KDC/;
+print "It matches\n" if /KDC.*KDC.*KDC.*certutil.*KDC/;
