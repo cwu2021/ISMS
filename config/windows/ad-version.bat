f04e86f761e7472f8736f65499e44405d9a265f9
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-23 12:51:10 +0800

    KDC certificate information.

--- config/windows/ad-version.bat
+++ config/windows/ad-version.bat
@@ -2,3 +2,6 @@
 REM https://docs.microsoft.com/zh-tw/troubleshoot/windows-server/identity/find-current-schema-version
 REM https://wiki.samba.org/index.php/AD_Schema_Version_Support
 dsquery * "cn=schema,cn=configuration,dc=contoso,dc=local" -scope base -attr objectVersion
+REM KDC certificate information.
+certutil -scinfo
+certutil -dcinfo
