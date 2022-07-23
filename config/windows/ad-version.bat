ec6b42ba9c149a215df7f2dee2323bcdabeee988
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-23 13:27:48 +0800

    references about KDC errors.

--- config/windows/ad-version.bat
+++ config/windows/ad-version.bat
@@ -5,3 +5,8 @@ dsquery * "cn=schema,cn=configuration,dc=contoso,dc=local" -scope base -attr obj
 REM KDC certificate information.
 certutil -scinfo
 certutil -dcinfo
+REM Event ID 29 — KDC Certificate Availability https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-R2-and-2008/cc734096(v=ws.10)?redirectedfrom=MSDN
+REM KDC Certificate Availability https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-R2-and-2008/cc733882(v=ws.10)
+REM Key Distribution: KDC, Certification, CA https://sls.weco.net/node/10687
+REM 智慧卡疑難排解 https://docs.microsoft.com/zh-tw/windows/security/identity-protection/smart-cards/smart-card-debugging-information
+REM 有關2003及2008 ad server上KDC的錯誤訊息? https://ithelp.ithome.com.tw/questions/10153686
