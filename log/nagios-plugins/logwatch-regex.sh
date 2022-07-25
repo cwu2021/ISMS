c29a5363577cb9357c2a835581e3f82bb20a0e4a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-25 10:51:06 +0800

    Kerberos V5 and cert issue.

--- log/nagios-plugins/logwatch-regex.sh
+++ log/nagios-plugins/logwatch-regex.sh
@@ -3,3 +3,4 @@
 # Windows event log of KDC error for smartcard.
 print "It matches\n" if /KDC.*KDC.*KDC.*certutil.*KDC/;
 # CA憑證服務已移除，DC裡PKI的CA物件已删除。
+# 目前Kerberos V5是可以選擇使用RSA加密，但是沒有提到憑證，但是有些說明文件又說憑證是用在驗證客戶端的身分，所以有點混亂。
