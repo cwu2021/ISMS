d8b812492c71b27d9bfaf1c3638d67e4b5286567
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-28 21:35:45 +0800

    hotter this year.

--- log/nagios-plugins/sipcmd/temper_trigger.sh
+++ log/nagios-plugins/sipcmd/temper_trigger.sh
@@ -8,6 +8,8 @@ sipport=
 declare -i temp=`/your/temper/code.sh`
 #if [ $temp -gt 15 ];then
 # modified due to high humidity after typhoon, Aug 10, 2021.
-if [ $temp -gt 25 ];then
+# if [ $temp -gt 25 ];then
+# modified due to high temperature this year , Jul 28, 2022.
+if [ $temp -gt 26 ];then
 sipcmd -u $usernum -c $userpass -P sip -w $sipserver:$sipport -x "c5551;ws3000;vsiptest.wav;h"
 fi
