12647fd8a9b6956435b725e3753e949dcede5fc9
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-08-02 10:55:13 +0800

    local checks as nrpe plugins.

--- log/nagios-plugins/check_localvar.sh
+++ log/nagios-plugins/check_localvar.sh
@@ -11,3 +11,14 @@ myps=`your/local/exe`
         statustxt=CRITICAL
     fi
     echo "$status localvar myps=$status;0;1;0; $statustxt - $myps result text"
+    
+myps=`/usr/local/bin/check_load -w 1,0.6,0.3 -c 2,1.2,0.6 | awk '{print $1}'`
+
+    if [ $myps != OK ] ; then
+        status=1
+        statustxt=WARNING
+    else
+        status=0
+        statustxt=OK
+    fi
+    echo "$status myLoad myps=1.0;0.6;0.3;0; $statustxt - server_load"
