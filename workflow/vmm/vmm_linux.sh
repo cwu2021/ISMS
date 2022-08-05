d356051c434d84c8c0b372a70790753f04681666
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-08-05 16:15:50 +0800

    shutdown for linux vm, maybe better than init.

--- workflow/vmm/vmm_linux.sh
+++ workflow/vmm/vmm_linux.sh
@@ -5,3 +5,7 @@ mkdir -p /VMs/linux
 cd /VMs/linux && ftp -V https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-virt-3.6.0-x86_64.iso
 # edit /etc/{pf,sysctl,dhcpd,vm}.conf
 rcctl enable vmd && rcctl enable dhcpd && rcctl set dhcpd flags vether0
+
+# shutdown for linux vm, maybe better than init 0, but still need to check if vm's really stopped.
+# https://blog.gtwang.org/linux/how-to-shutdown-linux/
+shutdown -h now
