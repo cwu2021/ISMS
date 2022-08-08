f826bd60ad5cd6ff4f1a4912d905f52b49b6d831
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-08-08 18:23:31 +0800

    moda not web 3.0, cloudflare indeed.

---
+++ VAS/moda-scan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Ministry of Digital Affairs https://moda.gov.tw/
+# https://pentest-tools.com/website-vulnerability-scanning/website-scanner
+nslookup moda.gov.tw
+whois 104.18.22.126
+# OrgName:        Cloudflare, Inc.
+# OrgId:          CLOUD14
+# Address:        101 Townsend Street
+# City:           San Francisco
+# StateProv:      CA
