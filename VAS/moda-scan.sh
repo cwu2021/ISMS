61d0f8ec04a1c483a1477e2a47226ec9091f1be1
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-08-08 18:25:23 +0800

    ptt comments of moda

--- VAS/moda-scan.sh
+++ VAS/moda-scan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Ministry of Digital Affairs https://moda.gov.tw/
 # https://pentest-tools.com/website-vulnerability-scanning/website-scanner
+# PTT comments: https://www.ptt.cc/bbs/Gossiping/M.1659842984.A.B2C.html
 nslookup moda.gov.tw
 whois 104.18.22.126
 # OrgName:        Cloudflare, Inc.
