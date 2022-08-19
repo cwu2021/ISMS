8f4edbfa8bd1c2ce3d0576ee41bfead5c56ce35b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-08-19 10:14:07 +0800

    ip range for CDN vendors.

---
+++ analysis/geoip/CDN-ip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/LocationsOfEdgeServers.html
+# https://ip-ranges.amazonaws.com/ip-ranges.json
+# https://www.cloudflare.com/zh-tw/ips/
+# https://www.cloudflare.com/ips-v4
