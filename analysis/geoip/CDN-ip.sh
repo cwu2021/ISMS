682c98fd526b5007eb691ec91f2d91ad1e2a292b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-08-22 09:31:29 +0800

    REST and WAF via CDN.

--- analysis/geoip/CDN-ip.sh
+++ analysis/geoip/CDN-ip.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/ksh
 # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/LocationsOfEdgeServers.html
 # https://ip-ranges.amazonaws.com/ip-ranges.json
 # https://www.cloudflare.com/zh-tw/ips/
 # https://www.cloudflare.com/ips-v4
+cat >> /etc/pf.conf << EOF
+table <cloudflare> persist file "/opt/ips-v4"
+block in on $ext_if proto tcp from any to $ext_ip port 443
+pass in on $ext_if proto tcp from <cloudflare> to $ext_ip port 443 rdr-to $internal_server port 443
+EOF
+# WAF for country rules can be applied on CDN security settings.
