a523e4d2a9ea891a4c90920012e9171df6de5965
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-05 16:25:02 +0800

    nft check blocked ip via sshguard.

--- log/nftables-watch.sh
+++ log/nftables-watch.sh
@@ -2,3 +2,6 @@
 # monitor nftables like pflog in bsd.
 # https://opensource.com/article/22/7/nftwatch-linux-firewall
 nftwatch
+# https://wiki.nftables.org/wiki-nftables/index.php/Quick_reference-nftables_in_10_minutes
+# check blocked ip via sshguard.
+nft list tables
