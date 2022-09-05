67abd3bd7fb2325a9ed1991d5ddce9bb31436534
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-05 16:04:11 +0800

    Zimbra proxy is a paid nginx proxy.

--- workflow/CDN/postfix-relay-ssl.sh
+++ workflow/CDN/postfix-relay-ssl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Zimbra proxy is a paid solution which utilizes nginx proxy,
+#  which implies that pops port 995, imaps port 993, smtps port 587 can be set. 
 # postfix relay with iptables input block. opensmtpd !for gmail relay host smtps:// . changing external gw ip useless to unblock gmail.
 # google workspace same mx as gmail...
 # https://wiki.centos.org/HowTos/postfix_sasl#Configuring_SSL.2FTLS_in_postfix
