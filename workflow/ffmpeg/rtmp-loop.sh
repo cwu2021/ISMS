b278124bc92c918e0b735323f18cc9f8bc6245cf
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-08 10:06:09 +0800

    backup stream turned on to resume broadcast.

--- workflow/ffmpeg/rtmp-loop.sh
+++ workflow/ffmpeg/rtmp-loop.sh
@@ -10,11 +10,14 @@ export LD_LIBRARY_PATH=/src/ffmpeg-2.6.2-1.el6.i686/lib
 
 folder='/myvideo/720p'
 output='rtmp://my.rtmp.server:1935/live/720p.stream'
+backup='rtmp://b.rtmp.server:1935/live/720p.stream'
 
 while true; do
 cd $folder
 ls | while read source
 do
-/src/ffmpeg-2.6.2-1.el6.i686/bin/ffmpeg -re -i $source -acodec copy -vcodec copy -bufsize 8000k -f flv $output < /dev/null
+# /src/ffmpeg-2.6.2-1.el6.i686/bin/ffmpeg -re -i $source -acodec copy -vcodec copy -bufsize 8000k -f flv $output < /dev/null
+# backup stream turned on to resume broadcast.
+/src/ffmpeg-2.6.2-1.el6.i686/bin/ffmpeg -re -i $source -acodec copy -vcodec copy -bufsize 8000k -f flv $output -acodec copy -vcodec copy -bufsize 8000k -f flv $backup < /dev/null
 done
 done
