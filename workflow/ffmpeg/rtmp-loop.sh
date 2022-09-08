6ea98272b5a89e4e81f125ac5ea70b78eb3a039e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-08 11:00:55 +0800

    WAN quality observation for streaming.

--- workflow/ffmpeg/rtmp-loop.sh
+++ workflow/ffmpeg/rtmp-loop.sh
@@ -19,7 +19,7 @@ do
 # /src/ffmpeg-2.6.2-1.el6.i686/bin/ffmpeg -re -i $source -acodec copy -vcodec copy -bufsize 8000k -f flv $output < /dev/null
 # backup stream turned on to resume broadcast.
 # 至於那個備援能起到多大作用，還是取決於youtube平台的實作是否完善，用戶就是照他設計的設定來做。但若網路質量不足，這樣的消耗反而會播不順 (沒起到備援作用反而過度消耗)。
-
+# 網路實際品質 (不是帳面頻寬多大，什麼幾百M幾G)，觀察直播就是跟$$直接相關：按用量計價的，就比固定費用吃到飽的來的穩。高單價的比低單價的穩。AB兩點專線最穩。這些差異在隨便上網不會察覺，而LIVE直播這種要求即時且穩定的品質，就會有差。
 /src/ffmpeg-2.6.2-1.el6.i686/bin/ffmpeg -re -i $source -acodec copy -vcodec copy -bufsize 8000k -f flv $output -acodec copy -vcodec copy -bufsize 8000k -f flv $backup < /dev/null
 done
 done
