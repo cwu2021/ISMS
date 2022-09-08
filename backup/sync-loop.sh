e6bcdb3362a02b5d8bb6db0ba5a057302a07349e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-08 10:09:31 +0800

    check rsync log via journalctl.

--- backup/sync-loop.sh
+++ backup/sync-loop.sh
@@ -2,6 +2,12 @@
 
 LANG=en_US
 
+# check rsync log via journalctl.
+journalctl -ef -t rsyncd
+# e.g. /uploads/assets/ 底下的文件/目錄
+# 1. 現在每日的同步僅僅會把當天和前一天的檔案同步到backup主機
+# 2. 如果舊的資料夾裡面的文件有變動, 就必須另外處理
+
 while true
 do
 echo "`date` Let's go!!" >> loop.log
