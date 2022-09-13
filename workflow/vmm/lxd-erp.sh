2ef9f18bafca0c004255da25bd36c3250ea8a1e5
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-13 10:14:07 +0800

    Using LXD to host your own ERP, cost-effectively

---
+++ workflow/vmm/lxd-erp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# https://canonical.com//blog/using-lxd-to-host-your-own-erp-cost-effectively
+# A small garment and textile manufacturer located in Shanghai, China was looking to minimise the costs of their infrastructure, so they focused on finding open-source tools to fulfil their needs. They chose ERPNext to manage their business processes.
