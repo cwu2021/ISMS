1b6756022e3d0141440098bade94aae2eb4d3ad9
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-18 23:33:46 +0800

    purge bin log via mysql prompt.

--- backup/ssh-mysqldump.sh
+++ backup/ssh-mysqldump.sh
@@ -2,3 +2,5 @@
 # https://stackoverflow.com/questions/40024280/mysqldump-via-ssh-to-local-computer
 mysqldump --all-databases > backup/alldb-`date +%Y%m%d`.sql
 ssh root@ipaddress "mysqldump -u dbuser -p dbname | gzip -9" > dblocal.sql.gz 
+# purge bin log via mysql prompt
+# > purge binary logs before '2021-09-01 23:59:59';
