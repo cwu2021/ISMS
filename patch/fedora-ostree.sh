1131d9d492030d308326e3e094d290d450413623
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-24 13:16:33 +0800

    rollback packages while 'Cannot find rpm nevra'.

--- patch/fedora-ostree.sh
+++ patch/fedora-ostree.sh
@@ -3,3 +3,10 @@
 systemctl is-active sshd
 rpm-ostree upgrade
 systemctl reboot
+# rollback/downgrade packages while 'Cannot find rpm nevra..'.
+# https://forums.fedoraforum.org/showthread.php?326163-dnf-history-rollback-can-t-find-rpm-s&p=1847962
+# https://robbinespu.gitlab.io/posts/rollback-with-fedora-repos-archive/
+dnf history
+dnf info fedora-repos-archive
+dnf install fedora-repos-archive
+dnf history undo 158
