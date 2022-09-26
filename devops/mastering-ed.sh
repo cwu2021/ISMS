ac00647338037fff53e60b6cbee6b13ade85c2ad
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-09-26 08:53:19 +0800

    Ed Mastery: The Standard Unix Text Editor.

---
+++ devops/mastering-ed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://www.amazon.com/-/zh_TW/Michael-W-Lucas-ebook/dp/B07BVBSDNZ/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=&sr=
+# Let me be perfectly clear: ed(1) is the standard Unix text editor. If you don’t know ed, you’re not a sysadmin. You're a mere dabbler. A dilettante. Deficient.
+# https://www.redhat.com/sysadmin/introduction-ed-editor
+ed
+# ?
+# a
+# [myconfig]
+# widget=True
