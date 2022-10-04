8fa4218bdeaa5769c9b2b153edec2e7d8553be1a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-10-04 13:03:24 +0800

    Sending a simple email via perl.

---
+++ log/nagios-plugins/perl-mailer.pl
@@ -0,0 +1,15 @@
+# http://blogs.perl.org/users/den/2022/09/sending-simple-email.html
+use Email::Sender::Simple qw(sendmail);
+use Email::Simple;
+use Email::Sender::Transport::Sendmail qw();
+use Try::Tiny;my $email = Email::Simple->create(
+ header=>[To=>$to, From=>$from,
+ Subject=>$subject],
+ body=>$body,
+);try {
+ sendmail($email,
+ {from=>$from,
+ transport=>Email::Sender::Transport::Sendmail->new});
+} catch {
+ print "Can't send mail: $_";
+}
