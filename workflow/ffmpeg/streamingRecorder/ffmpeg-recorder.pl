a50f37831daf623a11f4c9d1fccedd37480ce38f
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-10-27 13:39:38 +0800

    recorder code called by control.

---
+++ workflow/ffmpeg/streamingRecorder/ffmpeg-recorder.pl
@@ -0,0 +1,7 @@
+$output=$ARGV[0];
+$record_min=$ARGV[1];
+if (length ($record_min) == 1) {$record_min = '0'.$record_min;}
+$url='https://mychannel.url/first.m3u8';
+
+system "/my/bin/ffmpeg -re -i $url -acodec copy -absf aac_adtstoasc -vcodec copy -t 00:$record_min:00 -y $output";
+#system "timecode.sh $output $record_min";
