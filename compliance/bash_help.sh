25b87d18607c2f040fe40c9ecb410d678f7048d6
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-02 13:55:25 +0800

    Help facility to your Bash program

---
+++ compliance/bash_help.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/bash
+# following GNU coding guideline.
+# https://opensource.com/article/19/12/help-bash-program
+################################################################################
+#                              scriptTemplate                                  #
+#                                                                              #
+# Use this template as the beginning of a new program. Place a short           #
+# description of the script here.                                              #
+#                                                                              #
+# Change History                                                               #
+# 11/11/2019  David Both    Original code. This is a template for creating     #
+#                           new Bash shell scripts.                            #
+#                           Add new history entries as needed.                 #
+#                                                                              #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+#                                                                              #
+#  Copyright (C) 2007, 2019 David Both                                         #
+#  LinuxGeek46@both.org                                                        #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 2 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program; if not, write to the Free Software                 #
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|t|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
+################################################################################
+# Process the input options. Add options as needed.                            #
+################################################################################
+# Get the options
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+     \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+echo "Hello world!"
