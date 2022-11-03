280f4621c6eab78057d6797b49cee7dd7bf3747e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-03 10:30:22 +0800

    modern Perl object system framework

---
+++ devops/perlbook/moose.pl
@@ -0,0 +1,26 @@
+# apt install libmoose-perl
+# perl -le "print for @INC"
+# Intermediate Perl, p20.
+BEGIN { unshift @INC, '~/lib'; }
+use Horse;
+use v5.10;
+
+my $talking = Horse->new( name => "Mr. Ed" );
+$talking->color( "grey" ); #color setup
+
+say $talking->name, ' is colored ', $talking->color;
+
+=pod
+# https://stackoverflow.com/questions/3828205/how-do-i-enter-a-multi-line-comment-in-perl
+# Here's the Horse.pm from Intermediate Perl, p286.
+package Horse;
+use Moose;
+use namespace::autoclean;
+
+has 'name' => (is => 'rw' );
+has 'color' => (is => 'rw' );
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+=cut
