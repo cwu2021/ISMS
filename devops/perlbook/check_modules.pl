be92cf489915e4328c19cf2f71120a2cadf6ffb7
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-08 13:05:54 +0800

    darwin for osx with perl 5.8.8

--- devops/perlbook/check_modules.pl
+++ devops/perlbook/check_modules.pl
@@ -15,6 +15,7 @@ print "$_ " for @modules
 =pod
 C:\Strawberry\perl\bin>perl -le "print $^O"
 MSWin32
+(darwin for osx with perl 5.8.8)
 C:\Strawberry\perl\bin>perl check_modules.pl
 Algorithm::C3 Algorithm::Diff Alien::Build Alien::GMP Alien::Libxml2 Alt::Crypt:
 :RSA::BigInt App::cpanminus App::local::lib::Win32Helper App::module::version Ap
