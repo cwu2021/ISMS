ae35e08f3f8611fc44f610786f2ab1955cbff741
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-14 10:06:51 +0800

    Dell EMC OpenManage Repository

--- log/nagios-plugins/idrac_sel_cron.sh
+++ log/nagios-plugins/idrac_sel_cron.sh
@@ -1,3 +1,11 @@
+# Dell EMC OpenManage Repository
+# https://linux.dell.com/repo/community/openmanage/
+# https://linux.dell.com/repo/community/ubuntu/
+# Integrated Dell Remote Access Controller 9 RACADM CLI Guide
+# https://www.dell.com/support/manuals/zh-tw/idrac9-lifecycle-controller-v5.x-series/idrac9_5.xx_racadm_pub/hwinventory?guid=guid-c6325122-f01b-4125-a9f4-075040165e8f&lang=en-us
+# apt-get update very slow, stuck at "Waiting for headers" - Ask Ubuntu
+# https://askubuntu.com/questions/156650/apt-get-update-very-slow-stuck-at-waiting-for-headers
+
 racadm getsel > idrac_sel.current
 diff idrac_sel.current idrac_sel.old > /dev/null
 if [ $? == 1 ]
