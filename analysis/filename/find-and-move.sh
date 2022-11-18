39fac05af1cffe0b71a4d83e1cbe7ec4d1aa649d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-18 09:44:12 +0800

    grep text including subdirectories

--- analysis/filename/find-and-move.sh
+++ analysis/filename/find-and-move.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #  https://serverfault.com/questions/505949/move-files-to-another-directory-which-are-older-than-a-date 
 find /sourcedirectory -maxdepth 1 -mmin +365 -exec mv “{}” /destination/directory/ \;
+# How to use "grep" command to find text including subdirectories
+# https://askubuntu.com/questions/55325/how-to-use-grep-command-to-find-text-including-subdirectories
+grep -rl "string" /path
