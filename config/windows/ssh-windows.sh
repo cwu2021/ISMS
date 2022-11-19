4ff57e0bb7af0df1b45cdb09f8e3a6e47e827614
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-19 12:50:53 +0800

    cmdlets for powershell management.

--- config/windows/ssh-windows.sh
+++ config/windows/ssh-windows.sh
@@ -1,4 +1,5 @@
 @echo off
+REM Windows+R to run, +S to search.
 PowerShell
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
@@ -6,3 +7,6 @@ REM  Internet connection required to fetch Windows Update. (Error 0x8024402c)
 REM https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
 REM Cygwin also works on win10. 'binmode ntsec' for service value. finally mkpasswd for mapping local account. (Sep 21, 2021)
 ssh-host-config
+REM cmdlets for powershell management.
+get-computerinfo
+REM https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/?view=powershell-7.3
