632ff4936524040fd96c15c9f57aa1c8521874ef
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-26 13:36:33 +0800

    ssh -V for version check.

--- patch/ssh-compat.sh
+++ patch/ssh-compat.sh
@@ -4,3 +4,7 @@
 vi ~/.ssh/config
 # PubKeyAcceptedKeyTypes=+ssh-dss
 # HostKeyAlgorithms +ssh-rsa
+ssh -V
+uname -a
+# OpenSSH_6.9p1, LibreSSL 2.1.8 may not use key auth for ubuntu 22.04.
+# Darwin Kernel Version 15.6.0: Thu Jun 21 20:07:40 PDT 2018; root:xnu-3248.73.11~1/RELEASE_X86_64 x86_64
