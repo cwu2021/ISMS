a2067dae76ab32ea59290004e726758a143afe24
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-28 00:19:45 +0800

    Vacuuming journal logs before 2 days.

--- log/journalctl-purge.sh
+++ log/journalctl-purge.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Delete journal logs older than X days: sudo journalctl --vacuum-time=2days.
-#Delete log files until the disk space taken falls below the specified size:
+# Delete journal logs older than X days:
+sudo journalctl --vacuum-time=2d
+# Vacuuming done, freed 895.7M of archived journals from /var/log/journal/3acc037fe00849caa68a572f6534a2bd.
+# https://unix.stackexchange.com/questions/139513/how-to-clear-journalctl
+# Delete log files until the disk space taken falls below the specified size:
 sudo journalctl --vacuum-size=100M.
 # https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjP1ImM6o73AhVmyosBHTBbA4QQFnoECAgQAw&url=https%3A%2F%2Fubuntuhandbook.org%2Findex.php%2F2020%2F12%2Fclear-systemd-journal-logs-ubuntu%2F&usg=AOvVaw3oBlAx0JLt2HLbXg3psGfr
 # check journal logs and nftables rules for sshguard
