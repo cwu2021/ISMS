67a3481277de9179e0b55ea2cf7750ea2b137fe7
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-11-30 12:51:08 +0800

    Learn Windows PowerShell in a Month

--- config/windows/powershell-linux.sh
+++ config/windows/powershell-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu
 # https://blog.quickbreach.io/blog/powershell-remoting-from-linux-to-windows/
+# Learn Windows PowerShell in a Month of Lunches https://www.books.com.tw/products/0010809471
 sudo apt update
 sudo apt install wget apt-transport-https software-properties-common
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
