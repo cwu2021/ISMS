4ea3141c15767da3da1be54faa34060d5b531fde
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-10 09:48:45 +0800

    find then move plus.

--- analysis/filename/find-and-move.sh
+++ analysis/filename/find-and-move.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#  https://serverfault.com/questions/505949/move-files-to-another-directory-which-are-older-than-a-date 
-find /sourcedirectory -maxdepth 1 -mmin +365 -exec mv “{}” /destination/directory/ \;
+# https://serverfault.com/questions/505949/move-files-to-another-directory-which-are-older-than-a-date 
+# find /sourcedirectory -maxdepth 1 -mmin +365 -exec mv “{}” /destination/directory/ \;
+# this mv may be better: https://unix.stackexchange.com/questions/154818/how-to-integrate-mv-command-after-find-command
+find path_A -name '*AAA*' -exec mv -t path_B {} +
 # How to use "grep" command to find text including subdirectories
 # https://askubuntu.com/questions/55325/how-to-use-grep-command-to-find-text-including-subdirectories
 grep -rl "string" /path
