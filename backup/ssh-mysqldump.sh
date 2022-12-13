526230b8788cc145f6611b5428e118b59db67638
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-13 12:57:17 +0800

    mysqldump for remote db backup.

--- backup/ssh-mysqldump.sh
+++ backup/ssh-mysqldump.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/40024280/mysqldump-via-ssh-to-local-computer
+# https://code.yidas.com/mysqldump/
 mysqldump --all-databases > backup/alldb-`date +%Y%m%d`.sql
 ssh root@ipaddress "mysqldump -u dbuser -p dbname | gzip -9" > dblocal.sql.gz 
 # purge bin log via mysql prompt
 # > purge binary logs before '2021-09-01 23:59:59';
+# mysqldump for remote db backup. https://stackoverflow.com/questions/19552150/how-to-backup-mysql-database-on-a-remote-server
+mysqldump --host 192.168.1.15 -P 3306 -u dev -pmjQ9Y mydb > mydb.sql
