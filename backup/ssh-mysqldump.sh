3a05056e1a0e3317e9f511974b8627980bf85413
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-16 03:16:28 +0800

    Oracle Utilities Pocket Reference, etc.

--- backup/ssh-mysqldump.sh
+++ backup/ssh-mysqldump.sh
@@ -7,3 +7,9 @@ ssh root@ipaddress "mysqldump -u dbuser -p dbname | gzip -9" > dblocal.sql.gz
 # > purge binary logs before '2021-09-01 23:59:59';
 # mysqldump for remote db backup. https://stackoverflow.com/questions/19552150/how-to-backup-mysql-database-on-a-remote-server
 mysqldump --host 192.168.1.15 -P 3306 -u dev -pmjQ9Y mydb > mydb.sql
+# Oracle Utilities Pocket Reference: A Quick Reference for DBAs and Developers - Sanjay Mishra - Google Books
+# https://books.google.com/books/about/Oracle_Utilities_Pocket_Reference.html?id=D2WMC5XjoWYC
+# Knoppix Pocket Reference - Kyle Rankin - Google Books
+# https://books.google.com/books/about/Knoppix_Pocket_Reference.html?id=Lp_KAgAAQBAJ
+# jQuery Pocket Reference: Read Less, Learn More - David Flanagan - Google Books
+# https://books.google.com/books/about/jQuery_Pocket_Reference.html?id=bgyODwAAQBAJ
