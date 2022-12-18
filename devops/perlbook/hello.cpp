23431c12e11bebee1ff6def602a2f5ad68c2c7ac
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-18 10:10:04 +0800

    c++ and c#

---
+++ devops/perlbook/hello.cpp
@@ -0,0 +1,11 @@
+// https://www.w3schools.com/cpp/cpp_syntax.asp
+#include <iostream>
+using namespace std;
+
+int main() {
+          cout << "Hello World!";
+            return 0;
+}
+// then g++ hellp.cpp
+// https://books.google.com/books/about/C_6_0_Pocket_Reference.html?id=_z9KDAAAQBAJ
+// https://askubuntu.com/questions/1022007/is-knowledge-of-c-of-any-use-in-a-linux-system
