00a9032fe331d23ebe0aa1d0e0bd6e71e46c3847
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-19 22:07:39 +0800

    C#, F#, and .NET

--- devops/perlbook/hello.cpp
+++ devops/perlbook/hello.cpp
@@ -9,3 +9,13 @@ int main() {
 // then g++ hellp.cpp
 // https://books.google.com/books/about/C_6_0_Pocket_Reference.html?id=_z9KDAAAQBAJ
 // https://askubuntu.com/questions/1022007/is-knowledge-of-c-of-any-use-in-a-linux-system
+/* James Gosling, who created the Java programming language in 1994, and Bill Joy, 
+a co-founder of Sun Microsystems, the originator of Java, called C# an "imitation" of Java;
+Gosling further said that "[C# is] sort of Java with reliability, productivity and security deleted." */
+// https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+/* ASP.NET Web Apps | HTML, CSS, JavaScript, and C#
+.NET programming languages | C#, F#, and Visual Basic 
+Why OCaml? https://ocaml.org/about */
+// https://dotnet.microsoft.com/en-us/apps/aspnet/web-apps
+// https://dotnet.microsoft.com/en-us/languages
+// https://www.c-sharpcorner.com/UploadFile/84c85b/building-cross-platform-net-app-using-mono/
