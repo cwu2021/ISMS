7b74dd06215c8484819369f531b6e7813257281f
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-21 15:16:16 +0800

    System Program Problem Detected In Ubuntu

--- log/nagios-plugins/cups-disabled.sh
+++ log/nagios-plugins/cups-disabled.sh
@@ -5,3 +5,6 @@ systemctl disable cups-browsed
 # http://7thzero.com/blog/how-to-stop-ubuntu-20-04-from-auto-adding-network-printers
 systemctl stop cups
 systemctl disable cups
+# How To Fix System Program Problem Detected In Ubuntu
+# The crash reports are stored in /var/crash directory in Ubuntu. If you look in to this directory, you should see some files ending with crash.
+# https://itsfoss.com/how-to-fix-system-program-problem-detected-ubuntu/
