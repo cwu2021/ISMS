00981a806d4dbdeeaba35979ca3871e5cfb5dd7d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-22 10:29:48 +0800

    /run/user/$uid is created by pam_systemd

--- log/nagios-plugins/cups-disabled.sh
+++ log/nagios-plugins/cups-disabled.sh
@@ -8,3 +8,6 @@ systemctl disable cups
 # How To Fix System Program Problem Detected In Ubuntu
 # The crash reports are stored in /var/crash directory in Ubuntu. If you look in to this directory, you should see some files ending with crash.
 # https://itsfoss.com/how-to-fix-system-program-problem-detected-ubuntu/
+# What is this folder /run/user/1000?
+# /run/user/$uid is created by pam_systemd and used for storing files used by running processes for that user. These might be things such as your keyring daemon, pulseaudio, etc.
+# https://unix.stackexchange.com/questions/162900/what-is-this-folder-run-user-1000
