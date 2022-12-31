d32cb0ad02b39822bcc7f3fbc49d6fcac889e3f3
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-31 09:42:54 +0800

    SQL LIKE Examples

--- analysis/database/mysql-timestamp.sh
+++ analysis/database/mysql-timestamp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/1855082/getting-mysql-data-after-specific-date
+# https://www.w3schools.com/sql/sql_like.asp
 cat > mysql-timestamp.sql << EOF
 use MyDatabase;
 SELECT * FROM table WHERE TheNameOfTimestampColumn > '2009-01-28 21:00:00';
+SELECT * FROM Customers WHERE CustomerName LIKE 'a%';
 EOF
 mysql -p < mysql-timestamp.sql > myoutput.txt
