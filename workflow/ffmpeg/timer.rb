0385aef2818f357d9a5cb886fa04102a071bcfcd
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-04 17:10:22 +0800

    run cmd after some sleep.

---
+++ workflow/ffmpeg/timer.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+t = Time::new
+puts t.ctime
+puts t.strftime("It is %m/%d/%Y")
+puts t.strftime("Epoch is %s")
+# https://wizardforcel.gitbooks.io/w3school-ruby/content/22.html
+a = Time.now.strftime("%s").to_i
+b = Time.local(2023, 1, 2, 23, 30).strftime("%s").to_i
+# https://code-maven.com/convert-string-to-number-in-ruby
+sleep b-a
+system 'ffmpeg'
