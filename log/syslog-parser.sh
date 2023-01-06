f0102ddc8bec29f5b63a422e34acf0e8ac30d917
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-06 09:49:03 +0800

    sshd log on mac in /var/log/system.log

--- log/syslog-parser.sh
+++ log/syslog-parser.sh
@@ -2,3 +2,5 @@
 # NAS or router syslog can be forwarded to a central server via UDP 514, for example:
 # https://kb.synology.com/zh-tw/DSM/help/LogCenter/logcenter_server?version=6
 if grep "`date +%b\ %e\ %H`" /var/log/messages | grep user1; then echo "login found!";fi
+# sshd log on mac could be found in /var/log/system.log
+zgrep sshd /var/log/system.log.0.gz
