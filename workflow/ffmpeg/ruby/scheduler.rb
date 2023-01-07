75ec10df0e3cac441e2dd69a4c2ab5eb23b7ce01
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-07 10:37:17 +0800

    scheduler with Config.ini

---
+++ workflow/ffmpeg/ruby/scheduler.rb
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/questions/43216556/how-to-split-line-read-from-text-file
+f = File.open("Config.ini", "r")
+f.each_line do |line|
+# https://stackoverflow.com/questions/15993198/ruby-how-to-skip-ignore-specific-lines-when-reading-a-file
+        next if line.start_with? '#'
+        puts line.split(",")[2]
+end
