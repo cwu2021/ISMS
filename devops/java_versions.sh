d27095f2090f392c6809025d0ad32a7f486e5b9e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-09 00:39:15 +0800

    vncterm incompat with Firefox 51/jre 8u181 32bit.

--- devops/java_versions.sh
+++ devops/java_versions.sh
@@ -22,3 +22,11 @@ java -jar cal.jar
 java -XshowSettings:system -version
 # cgroups v2 support on Linux 
 # https://developers.redhat.com/articles/2022/04/19/java-17-whats-new-openjdks-container-awareness#why_container_awareness_is_important
+# Console Java Errors | Proxmox Support Forum
+# https://forum.proxmox.com/threads/console-java-errors.14777/
+# vncterm , not compatible with Firefox 51/jre 8u181 32bit.
+# https://www.oracle.com/webfolder/s/digest/8u181checksum.html
+# https://ftp.mozilla.org/pub/firefox/releases/51.0.1/win32/en-US/
+# https://support.mozilla.org/en-US/questions/1281896
+# https://www.java.com/zh-TW/download/help/enable_browser.html
+# https://www.java.com/zh-TW/download/help/firefox_java.html
