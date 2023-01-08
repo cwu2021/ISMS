d477f71d76f1c908f945921d27917cde936c0f0b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-08 23:57:38 +0800

    wday matching with config.

--- workflow/ffmpeg/ruby/scheduler.rb
+++ workflow/ffmpeg/ruby/scheduler.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/ruby
 # https://stackoverflow.com/questions/43216556/how-to-split-line-read-from-text-file
 f = File.open("Config.ini", "r")
+# https://entrision.com/blog/formatting-dates-in-ruby/
+wday = Time.now.strftime("%w")
 f.each_line do |line|
 # https://stackoverflow.com/questions/15993198/ruby-how-to-skip-ignore-specific-lines-when-reading-a-file
         next if line.start_with? '#'
-        puts line.split(",")[2]
+        puts "wday match!" if line.split(",")[2] == wday
 end
