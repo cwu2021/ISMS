83e2acba4835d2e41fd91f57e4fc034c56c1eb9e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-10 09:21:18 +0800

    java -jar jython.jar for shell

---
+++ devops/perlbook/jython_sample.py
@@ -0,0 +1,11 @@
+# https://www.jython.org/download.html
+# https://stackoverflow.com/questions/40802587/how-to-use-jython-standalone-jar-
+file-in-a-java-application
+# main.py
+# import hello
+print("Hello from main file")
+import os
+print os.name
+# hello.printhello()
+# print("I'm back in main")
+# then run with java -jar jython.jar main.py
