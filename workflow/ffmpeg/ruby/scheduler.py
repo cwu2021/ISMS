847823d9027a289eb4750e62246f40122bb03166
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-11 10:26:40 +0800

    scheduler sample in python

---
+++ workflow/ffmpeg/ruby/scheduler.py
@@ -0,0 +1,10 @@
+# https://www.w3schools.com/python/python_file_open.asp
+f = open("Config.ini", "r")
+# https://www.geeksforgeeks.org/convert-python-datetime-to-epoch/
+# https://entrision.com/blog/formatting-dates-in-ruby/
+#mday = Time.now.strftime("%w")
+#f.each_line do |line|
+# https://stackoverflow.com/questions/15993198/ruby-how-to-skip-ignore-specific-lines-when-reading-a-file
+#       next if line.start_with? '#'
+#       puts "wday match!" if line.split(",")[2] == mday
+#end
