da53944e74e0f20fadbfd2a929bcdf6ccb969546
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-11 10:28:26 +0800

    timer sample in python

---
+++ workflow/ffmpeg/ruby/timer.py
@@ -0,0 +1,7 @@
+# https://docs.python.org/zh-tw/3/library/datetime.html
+# https://www.programiz.com/python-programming/datetime/current-time
+# https://docs.python.org/3/library/time.html
+ 
+from datetime import date
+today = date.today()
+print today
