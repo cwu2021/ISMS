771d3187d609334b0f85289502c89ec409f8d637
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-23 09:58:31 +0800

    time stamps of dmesg in human readable format

--- log/syslog-parser.sh
+++ log/syslog-parser.sh
@@ -4,3 +4,6 @@
 if grep "`date +%b\ %e\ %H`" /var/log/messages | grep user1; then echo "login found!";fi
 # sshd log on mac could be found in /var/log/system.log
 zgrep sshd /var/log/system.log.0.gz
+# time stamps of dmesg in human readable format
+# https://www.linuxquestions.org/questions/linux-newbie-8/the-numbers-in-dmesg-4175425742/
+dmesg -T
