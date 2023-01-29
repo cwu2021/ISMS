a862cd977ff2e4cae8c8f6a2b9045acb4974f27a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-01-29 11:25:17 +0800

    Run x86 (Intel) images on Apple Silicon (M1) Macs

--- config/docker/docker_mac_host.sh
+++ config/docker/docker_mac_host.sh
@@ -5,3 +5,6 @@ brew tap homebrew/cask
 brew install --cask docker
 open /Applications/Docker.app
 # https://blog.yowko.com/homebrew-docker/
+docker run --rm -it --platform linux/amd64 rofrano/vagrant-provider:debian bash
+# if you use the --platform linux/amd64 parameter Docker will run the Intel version for you.
+# https://forums.docker.com/t/run-x86-intel-and-arm-based-images-on-apple-silicon-m1-macs/117123
