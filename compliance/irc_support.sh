21b7fbf4db50a9abd2bf45df3bd9f1b8ce31b195
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-02-01 09:48:20 +0800

    IRC support and Benefit of Gentoo

---
+++ compliance/irc_support.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt install irssi
+# https://libera.chat/guides/connect
+irssi -n <nickname> irc.libera.chat
+# /join #gentoo (then ctrl-p/n for window switch) /part /quit
+# https://wiki.gentoo.org/wiki/Benefits_of_Gentoo
