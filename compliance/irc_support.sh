83e652ecd837f992270be40039e255769a7ea6eb
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-02-02 01:32:07 +0800

    support response via IRC could be fast!

--- compliance/irc_support.sh
+++ compliance/irc_support.sh
@@ -3,4 +3,5 @@ apt install irssi
 # https://libera.chat/guides/connect
 irssi -n <nickname> irc.libera.chat
 # /join #gentoo (then ctrl-p/n for window switch) /part /quit
+# /whois <nickname>. support response in minutes; troll kicking out in seconds!
 # https://wiki.gentoo.org/wiki/Benefits_of_Gentoo
