e7e34a06e9c268e7e65f1bd38dffc6333178c046
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-02-17 10:19:03 +0800

    X11 with Docker on Mac

--- config/docker/docker_mac_host.sh
+++ config/docker/docker_mac_host.sh
@@ -8,3 +8,7 @@ open /Applications/Docker.app
 docker run --rm -it --platform linux/amd64 rofrano/vagrant-provider:debian bash
 # if you use the --platform linux/amd64 parameter Docker will run the Intel version for you.
 # https://forums.docker.com/t/run-x86-intel-and-arm-based-images-on-apple-silicon-m1-macs/117123
+# X11 with Docker on Mac
+# https://medium.com/@mreichelt/how-to-show-x11-windows-within-docker-on-mac-50759f4b65cb
+# https://www.xquartz.org/
+# https://www.macrumors.com/2012/02/17/apple-removes-x11-in-os-x-mountain-lion-shifts-support-to-open-source-xquartz/
