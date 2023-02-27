1556d53872256051016c2da61aaee78341fd9ac2
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-02-28 00:50:43 +0800

    ip banned by pf

--- ACL/pf-block.sh
+++ ACL/pf-block.sh
@@ -4,4 +4,8 @@ cat > /etc/pf.conf << EOF
 block all
 match out on egress nat-to egress
 pass in on egress proto tcp to egress port ssh
+# bad ip
+table <banned> persist file "/etc/mail/banned.list"
+block in from <banned>
+# for example, 194.180.50.0/24
 EOF
