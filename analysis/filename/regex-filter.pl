a0a8b690e64063839c2d6042a040c0e53f1a013d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-03-20 00:50:44 +0800

    Common lines between two files

--- analysis/filename/regex-filter.pl
+++ analysis/filename/regex-filter.pl
@@ -2,3 +2,7 @@
 # https://perldoc.perl.org/perlrequick
 
 "State is OK" =~ /State.*OK/; # Match
+
+# Common lines between two files
+# https://unix.stackexchange.com/questions/398142/common-lines-between-two-files
+`comm -12 <(sort file1) <(sort file2)`
