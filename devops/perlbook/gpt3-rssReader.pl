1e79311f1bd4089a57744e7533c23f07c924e082
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-05-07 01:05:25 +0800

    GPT3 rss reader in Perl

---
+++ devops/perlbook/gpt3-rssReader.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+# Here is a possible code for an rss reader in Perl running in terminal:
+
+use strict;
+use warnings;
+use XML::RSS;
+use LWP::Simple;
+use Term::ANSIColor;
+
+# Define the RSS feeds to read
+my @feeds = (
+"https://www.bbc.com/news/rss.xml",
+"https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/rss.xml",
+"https://feeds.reuters.com/reuters/topNews"
+);
+
+# Loop through each feed
+foreach my $feed (@feeds) {
+# Fetch the feed content
+my $content = get($feed);
+die "Could not get $feed" unless defined $content;
+
+# Parse the feed content
+my $rss = XML::RSS->new;
+$rss->parse($content);
+
+# Print the feed title and link
+print color("bold blue");
+print $rss->{channel}->{title}, "\n";
+print color("reset");
+print $rss->{channel}->{link}, "\n\n";
+
+# Print the feed items
+foreach my $item (@{$rss->{items}}) {
+# Print the item title and link
+print color("bold green");
+print $item->{title}, "\n";
+print color("reset");
+print $item->{link}, "\n\n";
+}
+
+# Print a separator
+print "-" x 80, "\n\n";
+}
