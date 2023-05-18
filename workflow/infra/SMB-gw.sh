0067f502bd7f6b9e72e7c40c3aac4ff5ee251ca8
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-05-19 00:50:13 +0800

    disable smb logging of printer err

--- workflow/infra/SMB-gw.sh
+++ workflow/infra/SMB-gw.sh
@@ -4,3 +4,6 @@ smbclient -U $myuser -W $mydomain -L $myserver
 # mount shares with then serve as SMB gateway.
 mount.cifs ... -o vers=1.0,...
 # read only, guest options set in /etc/samba/smb.conf.
+# https://askubuntu.com/questions/867243/what-is-warning-syslog-option-is-deprecated
+# https://forums.centos.org/viewtopic.php?t=58013
+# syslog = 0 in [global] to disable logging, e.g. printer errs 
