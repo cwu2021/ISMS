566a00732423a30f983b98a1ff73b4b3f19d81a8
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-08-24 09:57:24 +0800

    retrieving ts from url with wget

--- workflow/CDN/cdnwatch-pkg.pl
+++ workflow/CDN/cdnwatch-pkg.pl
@@ -5,3 +5,8 @@ use LWP::UserAgent;
 use HTTP::Request;
 use HTTP::Response;
 use Proc::Daemon;
+
+# retrieving ts from m3u url
+# apt install wget
+$URL = $BaseURL."playlist.m3u8?wowzasessionid=".$session_id;
+$command = `wget -t 3 $tsfile -O $savefile`;
