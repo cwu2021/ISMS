6429b64fae42e05e542d5a1c2443ba4335630310
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-08-26 01:47:44 +0800

    awk like operation in Perl

--- devops/perlbook/perl-grep3.pl
+++ devops/perlbook/perl-grep3.pl
@@ -11,3 +11,8 @@ while ( <> )
         {
         print if m/$regex/;
         }
+
+# https://askubuntu.com/questions/467896/how-to-correctly-invoke-awk-in-perl-to-print-out-the-3rd-last-field-in-a-line
+my $line="1 2 3 4 5 6 7";
+my @tab = split(/\s+/, $line);
+print $tab[-3],"\n";
