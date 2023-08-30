25bf25ed9791442840154e8c7e180e1f2b83be4d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-08-31 00:58:52 +0800

    Perl(proc::daemon) Download (RPM)

--- workflow/CDN/cdnwatch-pkg.pl
+++ workflow/CDN/cdnwatch-pkg.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 # perl v5.36.0 on Ubuntu 23.04 (lunar)
 # apt install libproc-daemon-perl
+# epel Perl(proc::daemon) Download (RPM)
+# yum install perl-Proc-Daemon
 use LWP::UserAgent;
 use HTTP::Request;
 use HTTP::Response;
