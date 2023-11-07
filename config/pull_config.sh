f5cde3b8f75d5824df80025fff6a399d6d3f7344
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-11-08 00:57:48 +0800

    package for ip addr

--- config/pull_config.sh
+++ config/pull_config.sh
@@ -3,3 +3,7 @@
 git clone https://github.com/cwu2021/ISMS.git
 cd ISMS
 git pull 
+
+# package for ip addr
+# https://askubuntu.com/questions/1212555/what-apt-package-installs-ip-addr-command
+apt install iproute2 -y
