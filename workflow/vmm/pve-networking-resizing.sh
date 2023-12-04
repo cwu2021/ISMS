62d38242e73352b77a92fe055dd6969fd757ac5a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-12-04 23:20:46 +0800

    pve4 dhcp then pct

--- workflow/vmm/pve-networking-resizing.sh
+++ workflow/vmm/pve-networking-resizing.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# https://pve.proxmox.com/wiki/Network_Configuration
+# pve4 dhcp then pct
+# http://download.proxmox.com/iso/
+# https://wiki.debian.org/NetworkConfiguration
+# http://download.proxmox.com/images/system/ # https://pve.proxmox.com/wiki/Network_Configuration
 # Network configuration can be done either via the GUI, or by manually editing the file /etc/network/interfaces, which contains the whole network configuration. 
 # Once the network is configured, you can use the Debian traditional tools ifup and ifdown commands to bring interfaces up and down.
 # Proxmox VE does not write changes directly to /etc/network/interfaces. Instead, we write into a temporary file called /etc/network/interfaces.new, this way you can do many related changes at once. This also allows to ensure your changes are correct before applying, as a wrong network configuration may render a node inaccessible.
