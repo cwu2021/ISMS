7fd9137cad607fb85650fba8288090db5e0b5ee2
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2023-12-23 01:08:17 +0800

    lxd config device add proxy

--- config/lxd/proxy-profile.sh
+++ config/lxd/proxy-profile.sh
@@ -9,3 +9,5 @@ lxc profile add container1 proxy-80
 lxc config show container1 -e
 lxc profile remove container1 proxy-80
 lxc profile delete proxy-80
+# https://discuss.linuxcontainers.org/t/forward-port-80-and-443-from-wan-to-container/2042/2
+lxc config device add mycontainer myport80 proxy listen=tcp:0.0.0.0:80 connect=tcp:127.0.0.1:80
