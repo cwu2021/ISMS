13d5c6a7a8a8e732150e7f9157575563ee8c20af
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-01-20 09:51:59 +0800

    cockpit setup on ubuntu

---
+++ log/cockpit-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt install cockpit -y
+# network manager issue for checking updates.
+nmcli con add type dummy con-name fake ifname fake0 ip4 1.2.3.4/24 gw4 1.2.3.1
+# secure with iptables
+cat >> /etc/rc.local << EOF
+#!/bin/bash
+iptables -A INPUT -p tcp -s <admin_ip> --dport 9090 -j ACCEPT
+iptables -A INPUT -p tcp --dport 9090 -j DROP
+exit
+EOF
+systemctl start rc-local
+# net availability 
+systemctl disable cockpit.socket
