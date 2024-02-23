5753aaa52a6d4ddb9248c5342717c8ef03ce4b9e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-02-24 01:35:33 +0800

    bionic i386 in lxd runs ok

--- config/lxd/snap-loop.sh
+++ config/lxd/snap-loop.sh
@@ -4,5 +4,5 @@ systemctl status snap.lxd.daemon
 # If /dev/loop* are created by snaps, just remove the snaps that created them, using snap remove <name_of_snap> . You can get a list of your installed snaps by running snap list .2019年12月11日
 # https://askubuntu.com/questions/1195388/how-to-remove-dev-loops
 snap remove lxd
-# use snap to follow the latest lxd. lxd or docker may not work properly on i386 distro. xenial x64 works fine with focal in lxd.
+# use snap to follow the latest lxd. lxd or docker may not work properly on i386 distro; bionic i386 in lxd runs ok. xenial x64 works fine with focal in lxd. 
 snap install lxd
