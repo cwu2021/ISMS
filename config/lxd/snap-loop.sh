94df479dac7cf816112711f34af055d89bbc4371
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-02-24 01:19:26 +0800

    xenial x64 works fine with focal in lxd.

--- config/lxd/snap-loop.sh
+++ config/lxd/snap-loop.sh
@@ -4,5 +4,5 @@ systemctl status snap.lxd.daemon
 # If /dev/loop* are created by snaps, just remove the snaps that created them, using snap remove <name_of_snap> . You can get a list of your installed snaps by running snap list .2019年12月11日
 # https://askubuntu.com/questions/1195388/how-to-remove-dev-loops
 snap remove lxd
-# use snap to follow the latest lxd. lxd or docker may not work properly on older distro like xenial.
+# use snap to follow the latest lxd. lxd or docker may not work properly on i386 distro. xenial x64 works fine with focal in lxd.
 snap install lxd
