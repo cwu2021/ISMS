20dae8b9662b965833b5f07fe4a856cf8d0998fb
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-03-04 11:18:16 +0800

    tls cert for nginx with cloudflare validation

---
+++ workflow/CDN/tls-nginx.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Cloudflare can offer valid cert from google for self-signed origin!
+# https://www.cloudflare.com/ips/
+# https://discussions.vtiger.com/discussion/173187/disabling-site-url
+
+# https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-16-04
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/ngin
+x-selfsigned.crt
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+cat > /etc/nginx/conf.d/myssl.conf << EOF
+server {
+listen 80;
+    listen                      443 ssl http2 default_server;
+    ssl_certificate             /etc/ssl/certs/nginx-selfsigned.crt;
+    ssl_certificate_key         /etc/ssl/private/nginx-selfsigned.key;
+    ssl_protocols               TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers   on;
+root /var/www/html/vtigercrm;
+index index.php index.html index.htm;
+server_name myssl.example.com mytls.example.com;
+client_max_body_size 100M;
+location / {
+try_files $uri $uri/ /index.php?$args;
+}
+
+systemctl restart php8.1-fpm
+systemctl reload nginx
