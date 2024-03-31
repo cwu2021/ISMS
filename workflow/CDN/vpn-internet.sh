210085cbbd38155f11dfeecc5e642c397d33e5ad
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-03-31 13:19:31 +0800

    proxy vs vpn

--- workflow/CDN/vpn-internet.sh
+++ workflow/CDN/vpn-internet.sh
@@ -6,3 +6,7 @@ iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 # smart projector , RGB BKL-72 keyboard (Oct 10,23 2023)
 # no bash history setup
 # https://farryhsu.com/how-to-evaluate-ctr-and-ad-perfromance/
+
+# https://surfshark.com/download/chrome
+# SOCKS is a proxy server, and works similarly to a VPN. The proxy server is much faster than a VPN. The main difference is that the proxy server isn’t encrypted. The data is still hidden and secure though, but the only part that is encrypted is the initial access to the proxy server. It is recommended to use SOCKS5 if you want better security. Though, the best security will be through a VPN.
+# https://www.quora.com/What-is-the-difference-between-socks-and-a-VPN
