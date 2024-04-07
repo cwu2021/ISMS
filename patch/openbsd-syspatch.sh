8020c9336b77613222ab8f6f447cb8895c5bc1a1
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-04-07 13:10:54 +0800

    OpenBSD Archive on EU mirror

--- patch/openbsd-syspatch.sh
+++ patch/openbsd-syspatch.sh
@@ -1,3 +1,5 @@
 #!/bin/ksh
 # bug occured for the 1st patch of 7.1! wait more days before patching... 
 syspatch
+# releases from 2.1 are archived on ftp eu.openbsd.org
+lynx https://ftp eu.openbsd.org/pub/OpenBSD
