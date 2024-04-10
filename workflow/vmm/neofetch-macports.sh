eda25901c6ee145e21ee595ac4a73c8e32ebf4ef
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-04-10 23:10:48 +0800

    neofetch and MacPorts

---
+++ workflow/vmm/neofetch-macports.sh
@@ -0,0 +1,11 @@
+# OSes in neofetch
+./neofetch --help
+# https://www.reddit.com/r/Windows11/comments/of65s9/custom_neofetch_logo_for_windows_11_by_me/?rdt=56339
+# Windows 11 release information
+# https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
+# https://blogs.windows.com/windows-insider/2023/10/04/announcing-windows-11-insider-preview-build-23560-dev-channel/
+# Homebrew Formulae (for Mac)
+# https://formulae.brew.sh/formula/wget
+# macOS 12 (Monterey) or higher is best and supported, 10.11 (El Capitan) - 11 (Big Sur) are unsupported but may work and 10.10 (Yosemite) and older will not run Homebrew at all. For 10.4 (Tiger) - 10.6 (Snow Leopard) see Tigerbrew.
+port -d selfupdate
+# https://stackoverflow.com/questions/27091831/how-slow-is-too-slow-for-macports-selfupdate
