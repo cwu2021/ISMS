dae7177948f29dd9c5d996e1106e4bb397b5f9e8
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-04-13 11:49:24 +0800

    mips spec of mt7628

---
+++ workflow/vmm/mt7628-mips.sh
@@ -0,0 +1,22 @@
+# mips to riscv after 2021 https://en.wikipedia.org/wiki/MIPS_architecture
+# https://openwrt.org/docs/techref/hardware/soc/soc.mediatek
+uname -a
+cat /proc/cpuinfo
+# https://stackoverflow.com/questions/947897/block-comments-in-a-shell-script
+:'
+Linux (none) 2.6.36 #1 Thu Apr 18 10:37:52 CST 2019 mips GNU/Linux
+system type		: MT7628
+processor		: 0
+cpu model		: MIPS 24Kc V5.5
+BogoMIPS		: 382.97
+wait instruction	: yes
+microsecond timers	: yes
+tlb_entries		: 32
+extra interrupt vector	: yes
+hardware watchpoint	: yes, count: 4, address/irw mask: [0x0ffc, 0x0ffc, 0x0ffb, 0x0ffb]
+ASEs implemented	: mips16 dsp
+shadow register sets	: 1
+core			: 0
+VCED exceptions		: not available
+VCEI exceptions		: not available
+'
