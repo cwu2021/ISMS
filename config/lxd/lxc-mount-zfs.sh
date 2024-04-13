5f06ae3f591f93aefb987de01f2759454accc2f4
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-04-14 04:30:01 +0800

    LXD Dir vs ZFS or BTRFS Performance

--- config/lxd/lxc-mount-zfs.sh
+++ config/lxd/lxc-mount-zfs.sh
@@ -4,3 +4,6 @@ zfs create web/myassets
 # zfs destroy web/myassets
 zfs set quota=500G web/myassets
 # lxc.mount.entry: /web/myassets mnt/zfs none bind 0 0
+
+# LXD Dir vs ZFS or BTRFS Performance
+# https://discuss.linuxcontainers.org/t/lxd-dir-vs-zfs-or-btrfs-instance-performance/13328/1
