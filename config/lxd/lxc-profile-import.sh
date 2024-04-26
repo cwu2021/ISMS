d1c9af1c8eb5f67dd324c4df23cd73545edaa717
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-04-27 01:03:23 +0800

    import physical or virtual machines to LXD

--- config/lxd/lxc-profile-import.sh
+++ config/lxd/lxc-profile-import.sh
@@ -4,3 +4,5 @@ lxc profile device add proxy-80 hostport80 proxy connect="tcp:127.0.0.1:80" list
 # lxc profile add focal proxy-80
 # lxc profile list
 lxc import focal.tar.gz r1
+# How to import physical or virtual machines to LXD instances
+# https://documentation.ubuntu.com/lxd/en/latest/howto/import_machines_to_instances/
