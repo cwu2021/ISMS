b1bc1eeff87f8b72145f0f10159b0260e56a960d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-04-27 10:37:05 +0800

    javac to jar

--- devops/java/Guess.java
+++ devops/java/Guess.java
@@ -1,8 +1,21 @@
 package com.example.guess;
-// opensource.com learn java
+// https://opensource.com/article/20/12/learn-java
 class Main {
  public static void main(String[] args) {
   System.out.println("That's right!");
   System.exit(0);
  }
 }
+/* 
+$ java -version
+$ java ./Guess.java
+$ mkdir -p com/example/guess
+$ echo "Manifest-Version: 1.0" > Manifest.txt
+$ javac src/Guess.java -d com/example/guess
+$ jar cfme Guess.jar \
+Manifest.txt \
+com.example.guess.Main \
+com/example/guess/Main.class
+$ jar tvf Guess.jar
+$ java -jar Guess.jar
+*/
