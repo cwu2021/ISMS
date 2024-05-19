52ecde0921ce949049dcdcc3de818fedad8951b2
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-05-19 14:44:13 +0800

    split -l instead of -b for txt

--- compliance/txt2epub.sh
+++ compliance/txt2epub.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-split -b 1M booktitle.txt booktitle_
+#split -b 1M booktitle.txt booktitle_
+# split -b may cause binary issue. -l for lines instead.
+split -l 18650 booktitle.txt booktitle_
 # then use online tools to convert booktitle_aa to booktitle_aa.epub
+# https://ebook.online-convert.com/convert/txt-to-epub
