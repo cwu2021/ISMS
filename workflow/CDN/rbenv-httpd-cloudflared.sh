2829789ca61c616032e551ffa362d5fd80f2231d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-05-26 09:59:33 +0800

    rbenv with httpd as well as cloudflared

---
+++ workflow/CDN/rbenv-httpd-cloudflared.sh
@@ -0,0 +1,10 @@
+rbenv install 1.8.7
+# /home/ubuntu/.rbenv/versions/1.8.7/bin/ruby millerquest.rb
+# https://linux.how2shout.com/3-ways-to-install-ruby-on-ubuntu-22-04-lts-jammy/#4_Install_Ruby_using_Rbenv
+# (archived site could be found at https://web.archive.org/web/20160306021319/http://www.beastwithin.org/users/wwwwolf/games/millerquest/)
+
+ruby -run -e httpd . -p 4567
+# https://www.seancdavis.com/posts/run-local-web-server-ruby/
+
+brew install cloudflared
+# https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/do-more-with-tunnels/trycloudflare/
