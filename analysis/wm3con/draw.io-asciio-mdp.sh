6b9ced4d5e77c573a52022a0344c2801da43d628
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-06-03 01:32:50 +0800

    diagrams with draw.io, asciio, mdp

---
+++ analysis/wm3con/draw.io-asciio-mdp.sh
@@ -0,0 +1,9 @@
+# https://github.com/nkh/P5-App-Asciio
+# draw.io | 使用 AsciiFlow 來畫關係圖的教學和心得
+# https://roulesophy.github.io/20201208-asciiflow-tutorial/
+
+# on focal:
+apt install asciio
+# (with remote X server)
+apt install mdp
+# (with github sample.md
