032ed17601b1752c7155858c7c993fa8a8cae955
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-06-07 01:09:36 +0800

    Not Your Dad's Database - Postgres

--- analysis/database/postgres-restore.sh
+++ analysis/database/postgres-restore.sh
@@ -1,3 +1,5 @@
+# Not Your Dad's Database - Postgres
+# http://www.linux-magazine.com/Issues/2024/283/PostgreSQL
 apt install postgresql
 su - postgres
 # roles may need to be created by createuserfirst!
