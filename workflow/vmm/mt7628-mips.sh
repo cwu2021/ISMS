69dc71efb2cf3d0c5012d69e65d2f357d29f9156
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-06-10 13:12:06 +0800

    Apple TV spec as well as Google TV

--- workflow/vmm/mt7628-mips.sh
+++ workflow/vmm/mt7628-mips.sh
@@ -1,6 +1,21 @@
 # mips to riscv after 2021 https://en.wikipedia.org/wiki/MIPS_architecture
 # Apple Airport is netbsd on MIPS; Darwin userland also runs netbsd.
 # https://zh.wikipedia.org/zh-tw/AirPort_Express
+# Apple TV spec: 
+:'A1427 或修正版 A 為 A1469
+推出年份：2012
+顏色：黑色
+HDMI（720p 或 1080p）
+10/100BASE-T 乙太網路
+Wi-Fi（802.11a/b/g/n/）
+光纖音訊
+紅外線接收器
+Micro-USB（適用於維修與支援）
+Apple Remote（鋁殼）
+請參閱 Apple TV（第 3 代）技術規格。
+
+華碩集團品牌 https://24h.pchome.com.tw/prod/DMAAA0-A900H4AN9
+'
 # Freebsd supports MIPS until 13.
 # https://openwrt.org/docs/techref/hardware/soc/soc.mediatek
 uname -a
