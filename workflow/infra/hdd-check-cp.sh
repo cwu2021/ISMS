0648e95288caf3c87b747ca6e0efcbf675b428ad
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-06-12 15:33:02 +0800

    cannot remount block device

--- workflow/infra/hdd-check-cp.sh
+++ workflow/infra/hdd-check-cp.sh
@@ -1,3 +1,5 @@
 # Create a test file with lots of zero bytes, or
 cp -a large folder
-#https://unix.stackexchange.com/questions/102613/create-a-test-file-with-lots-of-zero-bytes#102615
+# https://unix.stackexchange.com/questions/102613/create-a-test-file-with-lots-of-zero-bytes#102615
+
+# linux - mount: cannot remount block device /dev/sda5 read-write, is write-protected - Server Fault https://serverfault.com/questions/232705/mount-cannot-remount-block-device-dev-sda5-read-write-is-write-protected
