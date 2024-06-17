6d6816b4a8dd4d8e6d1e9a4eda5d980c00321ee4
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-06-17 12:52:18 +0800

    iptables -I INPUT -j sshguard may be required manually

--- ACL/epel-sshguard.sh
+++ ACL/epel-sshguard.sh
@@ -8,3 +8,7 @@ chmod 755 /etc/rc.local
 iptables -N sshguard
 iptables -A INPUT -j sshguard
 systemctl enable rc-local
+# 監控惡意行為並呼叫防火牆阻斷來源 自建開源碼網路攻擊阻擋系統 | 網管人
+#https://www.netadmin.com.tw/netadmin/zh-tw/technology/2FDCEED9F879437BAA6292D509A7C2FC
+iptables -I INPUT -j sshguard
+# the above may need to apply manually on ubuntu or docker hosts, in which iptables may be altered..
