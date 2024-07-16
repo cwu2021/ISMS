01d13cb0fa7fdbf7033511942e12c78ea251f433
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-07-17 00:53:13 +0800

    Backing up and restoring Redmine¶

---
+++ analysis/database/RedmineBackupRestore.sh
@@ -0,0 +1,9 @@
+# https://www.redmine.org/projects/redmine/wiki/RedmineBackupRestore
+# Database
+/usr/bin/mysqldump -u <username> -p<password> <redmine_database> | gzip > /path/to/backup/db/redmine_`date +%Y-%m-%d`.gz
+# Attachments
+rsync -a /path/to/redmine/files /path/to/backup/files
+# Restoring a database¶
+gunzip -c 2018-07-30.gz | mysql -u <username> --password <redmine_database>
+pg_restore -U <username> -h <hostname> -d <redmine_database> redmine.sqlc
+psql <redmine_database> < <infile>
