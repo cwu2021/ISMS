a7d8bcc36b4737a88f7b05cce6eabffc4e5910b1
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-07-19 01:40:00 +0800

    Google Pathways LLM from a Bash Script

--- devops/GPT/eliza-perl.sh
+++ devops/GPT/eliza-perl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo apt install libchatbot-eliza-perl 
 /usr/share/doc/libchatbot-eliza-perl/examples/simple
+# Google Pathways LLM from a Bash Script
+# https://www.linux-magazine.com/Issues/2024/282/Bash-PaLM-Shell
