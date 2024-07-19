758e65e6f86a106a5831ea296f4b8c051602abc7
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-07-19 12:16:56 +0800

    Mastodon - Twitter alternative via Perl

--- compliance/irc_support.sh
+++ compliance/irc_support.sh
@@ -5,3 +5,5 @@ irssi -n <nickname> irc.libera.chat
 # /join #gentoo (then ctrl-p/n for window switch) /part /quit
 # /whois <nickname>. support response in minutes; troll kicking out in seconds!
 # https://wiki.gentoo.org/wiki/Benefits_of_Gentoo
+# Mastodon - Twitter alternative via Perl to write into Fediverse
+# https://alashazam.wordpress.com/2024/07/09/perl-script-to-write-into-the-fediverse-and-nostr/comment-page-1/#comments
