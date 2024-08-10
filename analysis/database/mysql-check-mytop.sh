30038ef295c40d705114245568fae47251e9d167
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-08-11 02:36:24 +0800

    repair mysql tables then restart

--- analysis/database/mysql-check-mytop.sh
+++ analysis/database/mysql-check-mytop.sh
@@ -2,3 +2,5 @@
 # DON'T install mytop package with mariaDB! The latter has its own mytop, which conflicts!
 mysqlcheck -a --all-databases
 mytop
+# repair mysql tables then restart
+mysqlcheck -A --auto-repair ; service mysqld restart
