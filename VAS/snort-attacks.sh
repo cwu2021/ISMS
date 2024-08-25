d6da243b38307899d79076d417b8fa5e222cccb1
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-08-25 09:41:35 +0800

    modify existing iptables rules

--- VAS/snort-attacks.sh
+++ VAS/snort-attacks.sh
@@ -1,3 +1,7 @@
 sudo apt install snort
 # snort config of Ubuntu focal includes fast alerts for admin prohibited reject.
 iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
+
+# modify existing iptables rules
+iptables -L --line-number
+iptables -R INPUT 11 -p icmp -j ACCEPT
