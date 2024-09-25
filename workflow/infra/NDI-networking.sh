183ea5038f62427696ca74fc8650464a0a04b600
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-09-25 08:44:30 +0800

    ASI and Zixi fpr Broadcast Engineering

--- workflow/infra/NDI-networking.sh
+++ workflow/infra/NDI-networking.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # dedicated network is required to ensure NDI performance and security.
 ifconfig enp2s0 192.168.1.110 netmask 255.255.255.0
+# ASI and Zixi fpr Broadcast Engineering
+# Asynchronous serial interface - Wikipedia
+# https://en.wikipedia.org/wiki/Asynchronous_serial_interface
+# Harmonic Introduces Ellipse(TM) Encoder Product Line for Fixed Contribution and DSNG | Harmonic Inc.
+# https://investor.harmonicinc.com/news-releases/news-release-details/harmonic-introduces-ellipsetm-encoder-product-line-fixed/
+# 得安科技_Double Advance Technology Ltd.-Zixi Broadcaster https://www.2a.com.tw/product_1285671.html
