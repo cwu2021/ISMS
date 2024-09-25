322c0a14a9b010969e25b1a3d7d0a57e89e6d7f8
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-09-25 16:59:02 +0800

    VPN to intranet via vm or lxc

--- workflow/CDN/vpn-internet.sh
+++ workflow/CDN/vpn-internet.sh
@@ -3,6 +3,8 @@
 # NAT ubuntu 22.04
 sysctl net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+# VPN to intranet via vm or lxc
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 # smart projector , RGB BKL-72 keyboard (Oct 10,23 2023)
 # no bash history setup
 # https://farryhsu.com/how-to-evaluate-ctr-and-ad-perfromance/
