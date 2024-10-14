916ec5009cb5b0cbc55b69d94afb1d821959a33a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-10-15 07:20:27 +0800

    Check ca.crt on local directly

--- workflow/CDN/ssl-setup.sh
+++ workflow/CDN/ssl-setup.sh
@@ -5,6 +5,8 @@ openssl s_client -connect google.com:443 -tls1_2
 # CloudFront Custom SSL certificates
 # https://blog.cloudthat.com/cloudfront-custom-ssl-certificate/
 echo | openssl s_client -showcerts -servername gnupg.org -connect gnupg.org:443 2>/dev/null | openssl x509 -inform pem -noout -text
+# Check ca.crt on local directly 
+openssl x509 -inform pem -noout -text -in ca.crt
 # https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools
 # no need to include TrustedRoot.cer but DigiCertCA.cer to optimize bandwidth usage and page latency. Browsers have root CA but no intermediate ones.
 cat mysite.cer > mysite.pem
