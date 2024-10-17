cae49ca426aaf0a50a2dd4156778a260d7af20e8
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-10-18 01:03:32 +0800

    NTFS Filesystem: Alternate Data Stream (ADS)

--- config/windows/auto-cmd.bat
+++ config/windows/auto-cmd.bat
@@ -12,5 +12,9 @@ REM or via scheduled tasks
 schtasks
 REM attributes - How can I add NTFS metadata to any file? - Stack Overflow
 REM https://stackoverflow.com/questions/63701814/how-can-i-add-ntfs-metadata-to-any-file
+REM Windows NTFS Streams | Hostwinds https://www.hostwinds.com/blog/windows-ntfs-alternate-data-streams
+REM NTFS Filesystem: Alternate Data Stream (ADS) | by David Varghese | InfoSec Write-ups REMhttps://infosecwriteups.com/ntfs-filesystem-alternate-data-stream-ads-c0e4a2402563
+REM Finding and reading alternate data streams (ADS) with PowerShell on an NTFS partition · GitHub https://gist.github.com/chriselgee/bf41951d0b51d0ef9d2504a36921cd13
+REM How do I read Windows NTFS's Alternate Data Stream using Java's IO? - Stack Overflow https://stackoverflow.com/questions/33085253/how-do-i-read-windows-ntfss-alternate-data-stream-using-javas-io
 REM Map a network drive in Windows - Microsoft Support
 REM https://support.microsoft.com/en-us/windows/map-a-network-drive-in-windows-29ce55d1-34e3-a7e2-4801-131475f9557d
