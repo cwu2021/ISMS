e4dd9aa09cb5b17c2958c0f9af7060b2ace82115
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-10-23 00:43:37 +0800

    Sending and Receiving SRT Video Feeds - Viz Now

--- workflow/ffmpeg/SRT-ffmpeg-NDI.sh
+++ workflow/ffmpeg/SRT-ffmpeg-NDI.sh
@@ -39,3 +39,6 @@ nc  -u <dest> 10001 < /dev/random
 ./ffmpeg -f lavfi -re -i smptebars=duration=300:size=1280x720:rate=30 -f lavfi -re -i sine=frequency=1000:duration=60:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 1000k -g 30 -keyint_min 120 -profile:v baseline -preset veryfast -f mpegts "udp://127.0.0.1:1234?pkt_size=1316"
 
 ./srt-live-transmit udp://:1234 srt://:4201 -v
+# Sending and Receiving SRT Video Feeds - Viz Now
+# https://docs.vizrt.com/viz-now-launchpad/1.2/Sending_and_Receiving_SRT_Video_Feeds.html
+# https://www.haivision.com/blog/tag/play-pro/
