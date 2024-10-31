8c9946a0d8eac7ee4865c93a6000698747480c46
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-10-31 17:08:28 +0800

    wildcard: .*.* for logwatch

--- log/nagios-plugins/logwatch-regex.sh
+++ log/nagios-plugins/logwatch-regex.sh
@@ -1,6 +1,7 @@
 # https://blog.gtwang.org/perl/perl-course-notes-6/
 # 不指定比對目標，預設為 $_
 # Windows event log of KDC error for smartcard.
+# wildcard: .*.* https://docs.checkmk.com/latest/en/regexes.html
 print "It matches\n" if /KDC.*KDC.*KDC.*certutil.*KDC/;
 # CA憑證服務已移除，DC裡PKI的CA物件已删除。
 # 目前Kerberos V5是可以選擇使用RSA加密，但是沒有提到憑證，但是有些說明文件又說憑證是用在驗證客戶端的身分，所以有點混亂。
