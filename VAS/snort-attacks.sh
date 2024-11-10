14c1a8f93ecac367d853980250264caeff5aed77
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-11-10 11:07:31 +0800

    ping only scan for networks

--- VAS/snort-attacks.sh
+++ VAS/snort-attacks.sh
@@ -5,3 +5,6 @@ iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 # modify existing iptables rules
 iptables -L --line-number
 iptables -R INPUT 11 -p icmp -j ACCEPT
+
+# ping only scan for networks
+nmap;nmap -sn 192.168.0.0/24
