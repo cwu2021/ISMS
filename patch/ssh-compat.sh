9f76c1639d59b0a448352fa97dae78af38bf069a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-11-16 18:02:00 +0800

    ssh no matching host key type found

--- patch/ssh-compat.sh
+++ patch/ssh-compat.sh
@@ -13,3 +13,6 @@ uname -a
 # Darwin Kernel Version 15.6.0: Thu Jun 21 20:07:40 PDT 2018; root:xnu-3248.73.11~1/RELEASE_X86_64 x86_64
 # GSSAPIAuthentication no
 # https://serverfault.com/questions/75362/what-is-gssapi-with-mic
+# https://blog.alanwei.com/blog/2022/01/24/ssh-no-matching-host-key-type-found/
+ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa  user@host -p 22
+echo 'HostKeyAlgorithms=+ssh-rsa' >> ~/.ssh/config
