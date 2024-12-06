234f0d530444ffd366ed657d55e76378d6097e33
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-12-07 07:48:10 +0800

    Google Cast uses tcp port 8009 for device connection

--- workflow/ffmpeg/chromecast-npm.sh
+++ workflow/ffmpeg/chromecast-npm.sh
@@ -7,3 +7,4 @@ chromecast --host 192.168.1.100 status
 chromecast --host 192.168.1.100 play http://192.168.1.1/media/song.mp3
 sudo npm uninstall -g chromecast-cli
 sudo dnf remove npm nodejs nodejs-libs openssl nodejs-docs nodejs-full-i18n
+# Google Cast uses tcp port 8009 for device connection
