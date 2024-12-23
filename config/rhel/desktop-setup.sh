e1ad3497a09a2895f982d3ebf9ae9d5b5ffead06
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2024-12-23 10:24:08 +0800

    raspberry pi chinese input method

--- config/rhel/desktop-setup.sh
+++ config/rhel/desktop-setup.sh
@@ -1,3 +1,4 @@
 # raspi-config >> Locale > zh_TW&en_US.UTF8 # blog.gtwang.org
+# https://blog.gtwang.org/iot/raspberry-pi-chinese-input-method/
 sudo apt install scim-chewing
 sudo apt install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy
