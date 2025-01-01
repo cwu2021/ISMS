9ac386234d0a8394c7ea870f6bae876d693daa45
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-01-02 00:18:04 +0800

    grep for mixed encoding

--- analysis/filename/tag_check.sh
+++ analysis/filename/tag_check.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # check filename with TAG and STRING filtered.
 ls | grep -F [TAG] | grep -v -E 'string|STRING|Stringr'
+# grep for mixed encoding
+grep --text ^12 LogView* | grep --text Gap | wc -l
