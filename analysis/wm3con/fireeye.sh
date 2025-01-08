1094c6eb976e252365a9adef376dbce0b7c89d15
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-01-08 23:51:46 +0800

    Update fireeye.sh. Old Fireeye site not working.

--- analysis/wm3con/fireeye.sh
+++ analysis/wm3con/fireeye.sh
@@ -2,8 +2,8 @@
 # Re: How to disable the mouse pointer?
 # https://forums.raspberrypi.com/viewtopic.php?t=234879
 unclutter -idle 0 &
-# Full-screen mode: @chromium-browser --start-fullscreen . The browser starts in a
-pp. mode but expanded to full-screen. You can press the F11 key to break out of fu
-ll-screen mode.
-chromium-browser --start-fullscreen https://www.fireeye.com/cyber-map/threat-map.h
-tml
+# Full-screen mode: @chromium-browser --start-fullscreen . The browser starts in app. mode but expanded to full-screen. You can press the F11 key to break out of full-screen mode.
+# Fireeye site not working (Mar 18, 2023)
+#chromium-browser --start-fullscreen https://www.fireeye.com/cyber-map/threat-map.html
+#chromium-browser --disable-features=Translate --start-fullscreen https://stats.uptimerobot.com/Y9EQoCDx0r
+chromium-browser --disable-features=Translate --start-fullscreen https://threatmap.checkpoint.com
