798a147aa4e823083e7e617ef4dcba686ee559aa
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-01-12 00:24:53 +0800

    data-centric: 機器學習與人工智慧深度問答集

--- devops/GPT/eliza-perl.sh
+++ devops/GPT/eliza-perl.sh
@@ -3,3 +3,50 @@ sudo apt install libchatbot-eliza-perl
 /usr/share/doc/libchatbot-eliza-perl/examples/simple
 # Google Pathways LLM from a Bash Script
 # https://www.linux-magazine.com/Issues/2024/282/Bash-PaLM-Shell
+: <<'END'
+https://stackoverflow.com/questions/947897/block-comments-in-a-shell-script
+(Jan 6, 2024)
+https://www.ithome.com.tw/article/166815
+ - vs model AI
+ - data collection & curation
+
+https://www.tenlong.com.tw/products/9786263339248
+機器學習與人工智慧深度問答集：從基礎到專業，提升 AI 知識力的 30道深度思考題 (Machine Learning Q and AI: 30 Essential Questions and Answers on Machine Learning and AI)
+
+Part I：神經網路與深度學習
+Chapter 01：嵌入、潛在空間和表示
+Chapter 02：自監督學習
+Chapter 03：少樣本學習
+Chapter 04：樂透彩券假說
+Chapter 05：利用數據來減少過度適合
+Chapter 06：修改模型來減少過度適合
+Chapter 07：多GPU訓練範式
+Chapter 08：變換器的成功
+Chapter 09：生成式AI模型
+Chapter 10：隨機性來源
+Part II：電腦視覺
+Chapter 11：計算參數的數量
+Chapter 12：完全連接層與卷積層
+Chapter 13：視覺變換器（ViT）的大型訓練集
+Part III：自然語言處理
+Chapter 14：分布假說
+Chapter 15：文字的數據增強
+Chapter 16：自注意力
+Chapter 17：編碼器和解碼器風格的變換器
+Chapter 18：使用和微調預訓練的變換器
+Chapter 19：評估生成式大型語言模型
+Part IV：生產與部署
+Chapter 20：無狀態訓練和有狀態訓練
+Chapter 21：以數據為中心的AI
+Chapter 22：加速推理
+Chapter 23：數據分布偏移
+Part V：預測效能與模型評估
+Chapter 24：Poisson與有序迴歸
+Chapter 25：信賴區間
+Chapter 26：信賴區間vs.共形預測
+Chapter 27：合適的指標
+Chapter 28：k折交叉驗證中的k值
+Chapter 29：訓練集和測試集的不一致
+Chapter 30：有限標籤數據
+
+END
