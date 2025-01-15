b1823f0a05f9845db44f20f28419707186fd094e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-01-15 22:50:36 +0800

    run local LLM with ollama on ubuntu

---
+++ devops/GPT/ollama.sh
@@ -0,0 +1,10 @@
+#(run in lxc)
+sudo lxd init
+lxc launch ubuntu:24.04 ollama
+lxc shell ollama
+# https://ithelp.ithome.com.tw/articles/10348913
+# https://www.server-world.info/en/note?os=Ubuntu_24.04&p=ollama&f=1
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull gemma:2b
+ollama run gemma:2b
+# Note: Google ML learning doc reminds that LLMs hallucinate with all kinds of bias! (Jan 14, 2025)
