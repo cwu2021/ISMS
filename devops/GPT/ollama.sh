82b1be3eab9f48a1d21e38759548f451262907b3
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-01-21 01:34:27 +0800

    NCSA Launches DeltaAI Advanced Computing System

--- devops/GPT/ollama.sh
+++ devops/GPT/ollama.sh
@@ -8,4 +8,5 @@ curl -fsSL https://ollama.com/install.sh | sh
 ollama pull gemma:2b
 ollama run gemma:2b
 # Note: Google ML learning doc reminds that LLMs hallucinate with all kinds of bias! (Jan 14, 2025)
+# NCSA Launches DeltaAI Advanced Computing System https://www.admin-magazine.com/News/NCSA-Launches-DeltaAI-Advanced-Computing-System
 # Cloudera Launches RAG Studio https://www.admin-magazine.com/News/Cloudera-Launches-RAG-Studio
