e6f68eb612668085ea0c92d5087a68e4322e8b7c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-01-25 09:42:30 +0800

    mount.cifs for common SMB shares

--- workflow/infra/SMB-gw.sh
+++ workflow/infra/SMB-gw.sh
@@ -2,7 +2,8 @@
 # scan for older SMB shares.
 smbclient -U $myuser -W $mydomain -L $myserver
 # mount shares with then serve as SMB gateway.
-mount.cifs ... -o vers=1.0,...
+#mount.cifs ... -o vers=1.0,...
+mount.cifs //<ip>/<share> /<mount> -i user=<username>,pass=<password>,iocharset=utf8,rw
 # read only, guest options set in /etc/samba/smb.conf.
 # https://askubuntu.com/questions/867243/what-is-warning-syslog-option-is-deprecated
 # https://forums.centos.org/viewtopic.php?t=58013
