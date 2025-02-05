a823e487d40fd0c9d4fe7525bc5ad12f4db59107
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-02-05 10:22:39 +0800

    global stock market visualized in terminal like wttr.in

---
+++ analysis/database/terminal-stocks.sh
@@ -0,0 +1,5 @@
+#curl terminal-stocks.dev/ITC.NS,INFY.NS,WIPRO.NS
+#curl terminal-stocks.dev/market-summary | head
+curl terminal-stocks.dev/market-summary
+#terminal-stocks -m
+#https://github.com/shweshi/terminal-stocks
