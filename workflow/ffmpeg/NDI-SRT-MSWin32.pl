b2a6050dde23da1933fb1a935b42de854522aa29
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-02-21 23:00:00 +0800

    srt-live-server and FFMPEG-NDI both compile ok on jammy 22.04

---
+++ workflow/ffmpeg/NDI-SRT-MSWin32.pl
@@ -0,0 +1,24 @@
+# https://obsproject.com/download
+# https://learn.microsoft.com/zh-tw/windows-server/administration/windows-commands/msiexec
+`msiexec /i OBS-Studio-31.0.1-Windows-Installer` 
+# ndi.tv -> ndi.video
+
+# https://github.com/lplassman/FFMPEG-NDI
+=begin SRT-ffmpeg-NDI.sh
+Note: srt-live-server and FFMPEG-NDI both compile ok on jammy 22.04, but error on noble 24.04!
+https://stackoverflow.com/questions/36548940/yasm-nasm-not-found-or-too-old-use-disable-yasm-for-a-crippled-build
+sudo apt-get install yasm
+...
+=end SRT-ffmpeg-NDI.sh
+=cut
+
+=begin endnote
+https://forum.logik.tv/t/ndi-tools-4-6-download-links/3549
+NDI Tools 4.6 for Mac
+https://downloads.ndi.tv/Tools/NewTekNDIToolsForMacOS.dmg
+NDI Tools 4.6 for Windows
+https://downloads.ndi.tv/Tools/NDI%204%20Tools.exe
+If you’ve mistakenly installed NDI Tools 5, and it causes audio in Flame to be 
+all clicky and nasty, simply uninstall NDI Tools 5, and use the above inks to d
+ownload and install NDI Tools 4.6.
+=end endnote
