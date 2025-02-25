02ca505ae6943c2c923b0963961056e312722b63
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-02-26 01:17:47 +0800

    Ubuntu 18.04, 20.04, 22.04 Desktop can also be installed with Hyper-V templates

--- config/windows/wsl-install.bat
+++ config/windows/wsl-install.bat
@@ -2,3 +2,4 @@
 REM https://learn.microsoft.com/en-us/windows/wsl/install
 REM "Run as administrator", enter the command below, then restart your machine.
 wsl --install
+REM Ubuntu 18.04, 20.04, 22.04 Desktop can also be installed with Hyper-V templates.
