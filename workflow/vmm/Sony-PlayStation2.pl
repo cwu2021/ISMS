329cf13bd75e24ae1c2b1b1aea04c44a69ecc948
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-02-28 12:57:52 +0800

    Create Sony-PlayStation2.pl

---
+++ workflow/vmm/Sony-PlayStation2.pl
@@ -0,0 +1,5 @@
+# https://github.com/PCSX2/pcsx2
+$mips_emulator = `pcsx2-qt` ;
+
+=begin endnote
+(Feb 27, 2025, from Wiki) Playstation 2 (~2002, NT$1280) has only analog output (s-video, etc), and Sony also released its Linux console kit.
