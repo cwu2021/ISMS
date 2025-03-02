082569d9710431cbb7c2ecd064a699675f1ecf26
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-03-02 19:19:33 +0800

    start hyper-v service manually from GUI

---
+++ config/windows/hyperv-service-MSWin32.pl
@@ -0,0 +1,5 @@
+# start hyper-v service manually from GUI.
+`services.msc`;
+
+=begin endnote
+https://superuser.com/questions/858259/hyper-v-reports-that-the-hypervisor-is-not-running-how-to-start-the-hypervisor
