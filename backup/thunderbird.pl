3ec287dd65ffbc4101176c6dd19233eed6aacf29
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-03-05 13:36:30 +0800

    Mark All Messages Read Quickly in Mozilla Thunderbird

---
+++ backup/thunderbird.pl
@@ -0,0 +1,4 @@
+=begin endnote
+Mark All Messages Read Quickly in Mozilla Thunderbird
+To mark all messages read in a Mozilla Thunderbird folder quickly: Select the folder or any message in the folder. Press Shift+C.
+https://www.lifewire.com/mark-messages-read-thunderbird-1173204
