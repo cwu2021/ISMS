a002bf2760fc3f302c8080dd54cb268b0f04b483
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-03-06 16:23:43 +0800

    Google ML training module of from online 15hr course

--- devops/GPT/ollama.sh
+++ devops/GPT/ollama.sh
@@ -7,7 +7,7 @@ lxc shell ollama
 curl -fsSL https://ollama.com/install.sh | sh
 ollama pull gemma:2b
 ollama run gemma:2b
-# Note: Google ML learning doc reminds that LLMs hallucinate with all kinds of bias! (Jan 14, 2025)
+# Note: Google ML learning doc reminds that LLMs hallucinate with all kinds of bias! (Jan 14, 2025) : training module of from online 15hr course.
 # NCSA Launches DeltaAI Advanced Computing System https://www.admin-magazine.com/News/NCSA-Launches-DeltaAI-Advanced-Computing-System
 # Cloudera Launches RAG Studio https://www.admin-magazine.com/News/Cloudera-Launches-RAG-Studio
 # supremacy AI: ChatGPT, DeepMind 2024 Nobel mission shift
