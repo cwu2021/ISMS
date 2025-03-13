d0c6bc7140255a09975a8df9d597dceb303f326e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-03-13 20:36:35 +0800

    統計PTT看板推文 or 文章標題 熱門關鍵詞 for Windows and Linux on python

---
+++ analysis/SurveyMonkey/PttStatistics.pl
@@ -0,0 +1,11 @@
+# run python 2.7 and pip on focal:
+$python2 = `sudo apt install python2`;
+# stackoverflow/how-to-install-pip-for-python-2
+$pypa = `curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py`;
+$pip2 = `sudo python2 get-pip.py`;
+# stackoverflow/importerror-no-module-named-requests
+$modules = `sudo pip install requests jieba bs4`;
+
+=begin endnote
+git clone https://github.com/twtrubiks/PttStatistics
+python2 PttStatistics.py title gossiping 100
