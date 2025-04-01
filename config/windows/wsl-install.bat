3eac65114b9c43be3cbe69b625e1ed71e30d5d3b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-04-01 22:45:27 +0800

    wsl install Ubuntu-22.04 then move instance to another drive

--- config/windows/wsl-install.bat
+++ config/windows/wsl-install.bat
@@ -3,3 +3,8 @@ REM https://learn.microsoft.com/en-us/windows/wsl/install
 REM "Run as administrator", enter the command below, then restart your machine.
 wsl --install
 REM Ubuntu 18.04, 20.04, 22.04 Desktop can also be installed with Hyper-V templates.
+wsl --list --online
+wsl --install Ubuntu-22.04
+REM move instance to another drive
+wsl export
+wsl import
