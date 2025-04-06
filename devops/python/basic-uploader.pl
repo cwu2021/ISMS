a7ff8f422d0e436469059c128496ab2f3a592a7b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-04-06 11:11:04 +0800

    basic-uploader in python and notes

---
+++ devops/python/basic-uploader.pl
@@ -0,0 +1,7 @@
+=head1 basic-uploader
+git clone https://github.com/bndr/basic-uploader
+pip3 install aiohttp_jinja2
+cd basic-uploader; python3 run.py
+
+=begin endnote
+zen browser, ivon huang linux on android, ghostty, postmarketOS, unsupported browser.
