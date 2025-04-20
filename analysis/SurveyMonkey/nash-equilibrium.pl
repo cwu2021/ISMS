695540183a4c5db543b19bc84b12799bbe6eeaec
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-04-21 07:55:41 +0800

    Nash equilibrium of non-cooperate game

---
+++ analysis/SurveyMonkey/nash-equilibrium.pl
@@ -0,0 +1,4 @@
+$thesis = `wget https://www.cs.upc.edu/~ia/nash51.pdf`;
+# https://liaowc.github.io/blog/nash-equilibrium/
+=begin endnote
+non-cooperate game will eventually reach a balance state under ideal situation.
