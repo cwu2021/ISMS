30302fc2e9222ac31fd402979f38fde4dae2de20
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-04-26 01:10:04 +0800

    TWNIC Engagement Forum 2025

--- CTI/Dark_Web_RAG_IEC62443.pl
+++ CTI/Dark_Web_RAG_IEC62443.pl
@@ -3,3 +3,9 @@
  - Dark Web (V2->V3, nodes 3+) crawling w/ SOCKS5
  - Breech Forum -> RAG / LLM / CTI report (MISP)
  - IEC 62443-3-3
+(April 22-24, 2025) TWNIC Engagement Forum
+ - APNIC IPv6 >50%, honeynnet: dash.apnic.net
+ - ghost protocol & destruction plan.
+ - Trusted Notifier by CSC
+ - Content abuse moderation of Act 230
+ - dora.run/ai , Typosquating => digital ID = branding
