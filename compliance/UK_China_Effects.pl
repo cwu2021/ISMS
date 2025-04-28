20930ecb02b49837e0f9bab644ec63565791050d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-04-29 01:08:28 +0800

    UK: Autumn in the Heavenly Kingdom

---
+++ compliance/UK_China_Effects.pl
@@ -0,0 +1,8 @@
+=pod
+Autumn in the Heavenly Kingdom (2013/1st~2024/14th)
+ - 1851~1864 A.D.
+ - +France attacked BeiJing
+ - US civil war
+note: troops vs supplies
+ - 1:2.5 during World Wars
+ - 1:15 for modern military
