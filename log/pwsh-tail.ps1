defdee06c583a58e26d228f36e58586bdf8df4e5
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-05-20 00:33:54 +0800

    tail with powershell

---
+++ log/pwsh-tail.ps1
@@ -0,0 +1,4 @@
+# powershellcommands.com/powershell-tail-equivalent
+Get-Content -Path "C:\Logs\example.log" -Tail 0 -Wait
+# set-executionpolicy remotesigned
+# https://superuser.com/questions/106360/how-to-enable-execution-of-powershell-scripts
