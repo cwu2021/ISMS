9d590440b9583f9dd82c62b932a5215377f21975
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-06-10 01:11:49 +0800

    Process monitoring with Python and Telegram

---
+++ log/nagios-plugins/crashinspector.py
@@ -0,0 +1,4 @@
+# Process monitoring with Python and Telegram
+# https://www.linux-magazine.com/index.php/Issues/2025/294/Automated-Process-Monitoring
+# sudo apt install telegram-desktop
+# python crashinspector.py token:user_id process_name interval
