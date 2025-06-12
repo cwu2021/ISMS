fdd57bbb68aba71199f935100f068459c76fb7a7
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-06-12 19:09:16 +0800

    無限賽局 THE INFINITE GAME

--- analysis/SurveyMonkey/nash-equilibrium.pl
+++ analysis/SurveyMonkey/nash-equilibrium.pl
@@ -2,3 +2,5 @@ $thesis = `wget https://www.cs.upc.edu/~ia/nash51.pdf`;
 # https://liaowc.github.io/blog/nash-equilibrium/
 =begin endnote
 non-cooperate game will eventually reach a balance state under ideal situation.
+無限賽局：翻轉思維框架，突破勝負盲點，贏得你想要的未來 THE INFINITE GAME
+ - https://www.books.com.tw/products/0010969021?gad_source=1&gad_campaignid=22584964027&gbraid=0AAAAAD4DKPxcx88BE1K3t-1v2-29QdsTU&gclid=CjwKCAjw6ZTCBhBOEiwAqfwJd0G6TSVfbXbaqXuHY-hZqVEzykkcrB-8b-PZBPuJGQr5bO-CJG4qxBoCfGAQAvD_BwE
