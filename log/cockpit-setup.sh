d5147404530bb2e326dd97475ec877cfa75d314e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-06-14 12:36:28 +0800

    The Resurrection of bWatch: Beowulf HPC node monitoring

--- log/cockpit-setup.sh
+++ log/cockpit-setup.sh
@@ -14,4 +14,6 @@ systemctl start rc-local
 systemctl disable cockpit.socket
 # sysstat » ADMIN Magazine
 # https://www.admin-magazine.com/Archive/2024/81/Admin-as-a-service-with-sysstat-for-ex-post-monitoring
-#  Monitoring system statuses: Mission Control https://www.linux-magazine.com/Issues/2025/293/System-Monitoring
+# Monitoring system statuses: Mission Control https://www.linux-magazine.com/Issues/2025/293/System-Monitoring
+# With Beowulf, everything was new. Previous HPC systems all had proprietary tools to manage and monitor servers (nodes) in the cluster, so the tools for Beowulf clusters had to be developed, including those to monitor clusters. During the first few years of this Clusterian period of HPC, one of the key requirements was a simple tool to monitor nodes – something simple that would give the status of all nodes in the cluster.
+# https://www.admin-magazine.com/Articles/The-Resurrection-of-bWatch
