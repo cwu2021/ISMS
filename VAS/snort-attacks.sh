a7b6493f73de1ca7ed2c44d171d0d995bfe2937f
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-06-19 10:49:15 +0800

    Identify malicious traffic with Maltrail

--- VAS/snort-attacks.sh
+++ VAS/snort-attacks.sh
@@ -10,3 +10,4 @@ iptables -R INPUT 11 -p icmp -j ACCEPT
 nmap;nmap -sn 192.168.0.0/24
 
 # Pattern-matching tools for chasing down malicious software https://www.linux-magazine.com/Issues/2025/292/Malware-Minders
+# Identify malicious traffic with Maltrail https://www.admin-magazine.com/Archive/2025/86/Identify-malicious-traffic-with-Maltrail
