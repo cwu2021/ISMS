5acf885e6324b23da76413f800d015f864abd36e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-06-19 11:06:23 +0800

    Post-Quantum Cryptography with TutaCrypt

--- backup/encfs-backup.sh
+++ backup/encfs-backup.sh
@@ -4,3 +4,4 @@ fusermount -u ~/decrypted
 # How likely is it to have encFS not recognize a valid password? #442
 # https://github.com/vgough/encfs/issues/442
 reboot
+# Post-Quantum Cryptography with TutaCrypt https://www.admin-magazine.com/Archive/2025/86/Post-Quantum-Cryptography-with-TutaCrypt
