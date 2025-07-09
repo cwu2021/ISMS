5bc6fe5345f92917bdb9e1c0636c5e4bdfcd8e77
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-07-10 07:32:32 +0800

    grep characters with their unicode value

---
+++ analysis/filename/ugrep_for_utf-lite.pl
@@ -0,0 +1,11 @@
+$grep_utf16 = `sudo apt install ugrep`;
+=pod
+regular expression - How to grep characters with their unicode value ...
+       It takes the same options as grep but offers vastly more features,
+   such as: ugrep searches UTF-8/16/32 input and other formats. Option -Q
+   permits many other file formats to be searched, such as ISO-8859-1 to
+   16, EBCDIC, code pages 437, 850, 858, 1250 to 1258, MacRoman, and KIO8.
+   ugrep matches Unicode patterns by default (disabled with option -U).
+
+   unix.stackexchange.com/questions/523327/how-to-grep-characters-with-the
+   ir-unicode-value
