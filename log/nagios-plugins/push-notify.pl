f3c9160a08da1de9bbc021a252b2586e8b550d34
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-07-14 22:58:45 +0800

    Sending push notifications with ntfy

---
+++ log/nagios-plugins/push-notify.pl
@@ -0,0 +1,5 @@
+=pod
+ - Sending push notifications with ntfy https://www.linux-magazine.com/Issues/2025/297/Push-Notifications
+In this article, I'll explore ntfy [1] (pronounced "notify"), an open source project that receives alerts via HTTP through a REST API and converts them into push notifications for a smartphone app or a web browser on your desktop. Thanks to this API and its command-line interface, you can even enable your own shell scripts to send push notifications.
+ - https://www.linux-magazine.com/Issues/2025/297/Sending-Notifications
+If you use sensors with a Raspberry Pi or Arduino in your home network, you may want to get desktop notifications on your Linux PC whenever some interesting event is detected. You can send messages via SSH or through simple TCP connections and display them with notify-send.
