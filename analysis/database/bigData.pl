929c1ab6314409625a95a4864204340fb34a524c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-07-28 15:32:41 +0800

    Vs of Big Data

---
+++ analysis/database/bigData.pl
@@ -0,0 +1,3 @@
+=pod
+Vs of Big Data: Velocity, Volume, Variety, and then Varacity and Value. 
+ - Fixed volume can result in fixed cost, but high velocity with low variety or vice versa. High veracity and value can be achieved with good preprocessing for fixed volume.
