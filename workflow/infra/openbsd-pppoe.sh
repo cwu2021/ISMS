967e986540355a06c8dfc2df10dc691710fff51c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-04 18:42:24 +0800

    wifi infra design with vpn

--- workflow/infra/openbsd-pppoe.sh
+++ workflow/infra/openbsd-pppoe.sh
@@ -4,3 +4,9 @@ tail /var/log/messages
 # more tuning to pppoe may be required.
 man pppoe
 # LCP works fine after sdding inet6 settings in pppoe(4). LCP timeout happens for ipv4 only setting.
+:'
+wifi infra
+ - modem can be set as pppoe gateway = DMZ
+ - AP bridged into DMZ or NAT
+ - vpn can run behind NAT
+'
