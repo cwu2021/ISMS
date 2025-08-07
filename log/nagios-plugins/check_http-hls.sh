566c1cc677f2016bda26adf25ba05da03280d4e2
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-07 20:32:09 +0800

    check_httpv2 and check_cert with checkmk for websites and certs.

--- log/nagios-plugins/check_http-hls.sh
+++ log/nagios-plugins/check_http-hls.sh
@@ -1,2 +1,3 @@
 apt install monitoring-plugins
 $USER1$/check_http -H my.hls.host.url -u /live/playlist.m3u8 -M 15
+# check_httpv2 and check_cert with checkmk for better monitoring for websites and certificates. https://www.admin-magazine.com/Archive/2025/88/Better-monitoring-for-websites-and-certificates
