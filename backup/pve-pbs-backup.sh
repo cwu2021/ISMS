5f7461542c839cb260b2d1b02c887e70f6406726
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-07 20:35:29 +0800

    Proxmox Backup Server 4.0 with S3 support

--- backup/pve-pbs-backup.sh
+++ backup/pve-pbs-backup.sh
@@ -38,6 +38,7 @@ apt-get install proxmox-backup-server
 # After a reboot all is installed and started, don't forget to set the IP-address you want to use
 # and mounting the NFS-share you want to use as the storage for backups.
 # Put the NFS-share into /etc/fstab to automatically mount it at reboot.
+## Proxmox Backup Server 4.0: with S3-compatible object storage support
 dpkg -l | grep sshfs
 sshfs backup01:/data/backup /mnt/backup
 # or simpler backup via sshfs
