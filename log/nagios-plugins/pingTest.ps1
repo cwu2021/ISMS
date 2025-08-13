c38594205840d172c7c3fa74eb4f8826ebf82a26
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-14 01:53:32 +0800

    ipv6 can also be disabled from ncpa.cpl to prevent ISP autoconfig

--- log/nagios-plugins/pingTest.ps1
+++ log/nagios-plugins/pingTest.ps1
@@ -4,3 +4,4 @@ $ping = New-Object System.Net.Networkinformation.Ping
 1..254 | ForEach-Object {
     $ping.Send("192.168.0.$_") | Select-Object Address, Status, RoundtripTime
 }
+# ipv6 can also be disabled from ncpa.cpl to prevent ISP autoconfig, which may result in regional block and ipv4/ipv6 mis-config.
