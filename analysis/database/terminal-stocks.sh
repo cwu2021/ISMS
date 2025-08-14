31c08472f0b300a396fd8adeb0d785c04617fe4c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-15 01:26:07 +0800

    A data-oriented shell

--- analysis/database/terminal-stocks.sh
+++ analysis/database/terminal-stocks.sh
@@ -1,5 +1,6 @@
-#curl terminal-stocks.dev/ITC.NS,INFY.NS,WIPRO.NS
-#curl terminal-stocks.dev/market-summary | head
+# curl terminal-stocks.dev/ITC.NS,INFY.NS,WIPRO.NS
+# curl terminal-stocks.dev/market-summary | head
 curl terminal-stocks.dev/market-summary
-#terminal-stocks -m
-#https://github.com/shweshi/terminal-stocks
+# terminal-stocks -m
+# https://github.com/shweshi/terminal-stocks
+# A data-oriented shell https://www.linux-magazine.com/index.php/Issues/2025/296/Nushell
