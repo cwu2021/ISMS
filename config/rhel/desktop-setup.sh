4e0a5daa6fd6893a6c7db9f7373b8ded2712e75f
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-19 19:02:19 +0800

    mapleboard MP510

--- config/rhel/desktop-setup.sh
+++ config/rhel/desktop-setup.sh
@@ -2,3 +2,11 @@
 # https://blog.gtwang.org/iot/raspberry-pi-chinese-input-method/
 sudo apt install scim-chewing
 sudo apt install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy
+# mapleboard MP510
+:'
+https://www.ptt.cc/bbs/PC_Shopping/M.1657641681.A.43E.html
+→ menchian: 這顆SOC就電視盒很常用的 07/13 01:03
+推 moswu: 現在3000要買RPi其實有點難 07/13 01:35
+https://chihpindu.github.io/2022/04/%E8%87%AA%E6%9E%B6%E4%BC%BA%E6%9C%8D%E5%99%A8-mp510-50-%E6%A5%93%E8%91%89%E6%9D%BF/
+'
+
