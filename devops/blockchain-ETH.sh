b75bd9fb6f31446e8c984b421a70450ab8b70df9
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-25 18:04:29 +0800

    美國懷俄明州推出穩定幣FRNT

--- devops/blockchain-ETH.sh
+++ devops/blockchain-ETH.sh
@@ -14,3 +14,8 @@ sudo apt-get install ethereum
 # regular starup commands after private chain setup completely ..
 geth --datadir /home/petros/eth-evm/data/PrivateBlockchain init /home/petros/eth-evm/config/Genesis.json
 geth --datadir /home/petros/eth-evm/data/PrivateBlockchain --networkid 9999
+# https://www.ithome.com.tw/news/170723
+:'
+美國懷俄明州推出穩定幣FRNT
+懷俄明州是依據該州在2023年通過的穩定幣法令《Wyoming Stable Token Act》設立了懷俄明州穩定幣委員會（Wyoming Stable Token Commission），並由州長Mark Gordon兼任委員會主席。此一法令同時也要求FRNT必須具備至少102%的儲備比例，來提升代幣的穩定性及信任度，而FRNT則以美元及短期美國國債來維持2%的超額抵押。
+'
