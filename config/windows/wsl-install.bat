7c5a8582ee85ac1f7ab23bd31520d13a846edf1e
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-08-26 19:54:19 +0800

    Update wsl-install.bat

--- config/windows/wsl-install.bat
+++ config/windows/wsl-install.bat
@@ -8,4 +8,4 @@ wsl --install Ubuntu-22.04
 REM move instance to another drive
 wsl export
 wsl import
-::how to run RHEL 10 on Windows as a Windows Subsystem for Linux distribution using Red Hat Enterprise Linux image builder.
+:: how to run RHEL 10 on Windows as a Windows Subsystem for Linux distribution using Red Hat Enterprise Linux image builder.
