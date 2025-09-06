78e358a9dff1bb6ea3eacb85ea966eb032e06245
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-09-06 23:31:21 +0800

    Updated Debian 13: 13.1 released

--- patch/debian-update.pl
+++ patch/debian-update.pl
@@ -1,11 +1,10 @@
 =pod
 Updated Debian 12: 12.11 released
+ - Updated Debian 12: 12.12 released
  - Linux 6.1.137-1, included with Debian 12.11 is unable to load the "watchdog" and "w83977f_wdt" modules on the "amd64" architecture. This is a regression. This issue will be fixed in a forthcoming update. Users who rely on the watchdog functionality should disable their watchdog or avoid upgrading to this version of the kernel until a fix is
 available.
  - Rust in the Linux kernel https://www.linux-magazine.com/Issues/2025/295/Rust-in-the-Kernel
 Debian 13.0 Officially Released https://www.linux-magazine.com/Online/News/Debian-13.0-Officially-Released
+ - Updated Debian 13: 13.1 released
  - On Saturday 19 July 2025, the annual Debian Developers and Contributors Conference [1] came to a close. https://debconf25.debconf.org/
- - Over 443 attendees representing 50 countries from around the world came together for a combined 169 events (including some which took place
-during the DebCamp) including more than 50 Talks, 39 Short Talks, 5 Discussions, 59 Birds of a Feather sessions ("BoF" – informal meeting
-between developers and users), 10 workshops, and activities in support of furthering our distribution and free software, learning from our
-mentors and peers, building our community, and having a bit of fun.
+ - Over 443 attendees representing 50 countries from around the world came together for a combined 169 events (including some which took place during the DebCamp) including more than 50 Talks, 39 Short Talks, 5 Discussions, 59 Birds of a Feather sessions ("BoF" – informal meeting between developers and users), 10 workshops, and activities in support of furthering our distribution and free software, learning from our mentors and peers, building our community, and having a bit of fun.
