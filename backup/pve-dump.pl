52918261ab23a93b0b1b1776516d87ce8b75047d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-09-26 05:35:13 +0800

    Mounting Archives with ratarmount

--- backup/pve-dump.pl
+++ backup/pve-dump.pl
@@ -3,3 +3,4 @@ $mydir = "/path/to/my/dir" ;
 $task = `vzdump $vmid --compress lzo --dumpdir $mydir` ;
 # if sftp dump files stalled, vm network may be disconnected!
 print $task ;
+# The ratarmount archive mounting tool offers fast, random access to archived resources.
