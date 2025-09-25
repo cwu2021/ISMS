c5b99296ab7ef6671f6240fcd3436765b701ad95
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-09-26 05:29:33 +0800

    Identify malicious traffic with Maltrail

--- VAS/tcpdump_syntax.sh
+++ VAS/tcpdump_syntax.sh
@@ -1,3 +1,4 @@
 pftop -f syntax # (=tcpdump) 
 # https://stackoverflow.com/questions/23782333/tcpdump-filter-src-dst-port
 tcpdump -i any -vvv -s0 port 5060 and (not src port 5060 and not dst port 5061)
+# Maltrail is a free tool that can be a useful addition in your search for would-be attackers on your network.
