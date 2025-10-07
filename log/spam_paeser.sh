939289254f085c59fa10687906de00f56fb0acd9
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-10-07 19:17:05 +0800

    Proxmox Mail Gateway 9.0 released

--- log/spam_paeser.sh
+++ log/spam_paeser.sh
@@ -9,3 +9,4 @@ grep "You are in really big troubles therefore, you much better read" /var/log/s
 grep 183.111.115.4 /var/log/spamd | tee wankstortion/20221123_trapped_183.111.115.4.txt
 grep trouble /var/log/spamd | awk '{print $6}' | tr -d ':' | sort -u | grep -vc BLACK | tee -a wankstortion/20221123_campaign_ip_addresses.txt
 for foo in $troubles ; do grep $foo /var/log/spamd | tee -a wankstortion/20221123_campaign_log_extract.txt ; done
+# Proxmox Mail Gateway 9.0 released
