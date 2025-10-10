9f24e024bd3c6e2592221e8fd4dd0ad5d109a17c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-10-10 11:57:15 +0800

    USB2.0 HD detected on Windows 11 as HDMI input.

--- workflow/ffmpeg/video2hls.sh
+++ workflow/ffmpeg/video2hls.sh
@@ -9,4 +9,4 @@ ffmpeg -i input_video.mp4 \
 # https://www.mux.com/articles/how-to-convert-mp4-to-hls-format-with-ffmpeg-a-step-by-step-guide
 # with an HDMI USB dongle for input, Windows can act as an encoder with tor management. TEMPer USB dongle can do environmental measurement as well. 
 ffmpeg -list_devices true -f dshow -i dummy
-# USB UHD and audio can both be detected with both OBS 27 and ffmpeg essential on Windows 7 64-bit.
+# USB UHD and audio can both be detected with both OBS 27 and ffmpeg essential on Windows 7 64-bit. However, USB2.0 HD is used on Windows 11.
