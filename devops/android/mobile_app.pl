eaab1fcff5214fb69ebcdc46db0606c32f40f627
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-10-13 10:43:02 +0800

    Google開發者驗證新制遭F-Droid警告恐限縮開源應用

--- devops/android/mobile_app.pl
+++ devops/android/mobile_app.pl
@@ -2,3 +2,4 @@ $pkg = `wget network_survey.cidr_calculator..f-droid`;
 =pod
  - app check SOP available => v4.1 from MODA
  - phone and app check results
+ - Google開發者驗證新制遭F-Droid警告恐限縮開源應用 https://www.ithome.com.tw/news/171456
