ca63b3d018c4f86c04d580277c259a39f3031a6c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-10-14 12:29:17 +0800

    Trivy scanners for vulnerabilities, misconfigurations, and secrets with Ubuntu-centric guidance

--- VAS/nc-tcp-server.sh
+++ VAS/nc-tcp-server.sh
@@ -2,3 +2,6 @@
 # just for check compatibility
 nc -l <myip> -p <myport>
 # https://blog.gtwang.org/linux/linux-utility-netcat-examples/
+:'
+Trivy scanners for vulnerabilities, misconfigurations, and secrets with Ubuntu-centric guidance on performance tuning, security configurations, and scalability across Linux distributions.
+'
