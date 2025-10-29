b9731233c702c826ec12e50e35d3256744406aed
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-10-29 19:05:43 +0800

    How big data is used for patrol planning

---
+++ analysis/database/patrol.pl
@@ -0,0 +1,6 @@
+=pod
+https://leb.fbi.gov/articles/featured-articles/predictive-policing-using-technology-to-reduce-crime
+https://www.tandfonline.com/doi/full/10.1080/24751979.2024.2371781#abstract
+https://www.sciencedirect.com/science/article/pii/S2405844023081392
+How big data is used for patrol planning
+The Los Angeles Police Department (LAPD) tested the method under a controlled experiment. The project scientifically proved the model’s effectiveness. The city has a larger population and more complex patrol needs than Santa Cruz. Researchers established the experiment in the Foothill Division with a population of 300,000 people. They compared the predictive policing system with LAPD’s best practices.
