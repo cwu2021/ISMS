8db953e2c38d07addc5c74fc0563626fbbcbee3c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-10-31 01:27:19 +0800

    Homepage and the Warp AI

---
+++ workflow/CDN/Homepage-AI.pl
@@ -0,0 +1,3 @@
+=pod
+ - Simplify the chaos of self-hosted services with Homepage, a customizable dashboard with widgets that put service statistics at your fingertips.
+ - The Warp AI agent takes the guesswork out of working at the command line. We show you how to build a simple website with one prompt.
