f1cd5b0975f181727152f6bf441c158218017b95
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-11-20 16:08:30 +0800

    Proxmox Virtual Environment 9.1 available

--- config/lxd/pve-lxc.sh
+++ config/lxd/pve-lxc.sh
@@ -12,7 +12,10 @@ man pct
 # install old pve via kvm on new pve host, then restore the old lxc archive.
 # Storage across the network with iSCSI and Synology DiskStation Manager https://www.admin-magazine.com/Archive/2024/84/Storage-across-the-network-with-iSCSI-and-Synology-DiskStation-Manager
 # Automating deployments on Proxmox with OpenTofu and cloud-init https://www.admin-magazine.com/Archive/2024/84/Automating-deployments-on-Proxmox-with-OpenTofu-and-cloud-init
-# Celebrating 20 Years of Proxmox! https://forum.proxmox.com/threads/celebrating-20-years-of-proxmox.165655/
-# Proxmox Virtual Environment 8.4 https://forum.proxmox.com/threads/proxmox-ve-8-4-released.164821/
-# Proxmox Virtual Environment 9.0 with Debian 13 released
-# Beta release: Proxmox Datacenter Manager
+:'
+ - Celebrating 20 Years of Proxmox! https://forum.proxmox.com/threads/celebrating-20-years-of-proxmox.165655/
+ - Proxmox Virtual Environment 8.4 https://forum.proxmox.com/threads/proxmox-ve-8-4-released.164821/
+ - Proxmox Virtual Environment 9.0 with Debian 13 released
+ - Beta release: Proxmox Datacenter Manager
+ - Proxmox Virtual Environment 9.1 available
+'
