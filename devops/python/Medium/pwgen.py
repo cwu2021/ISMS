bbc6d438679f4f0271cb7471e05ec22db466e37d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2025-11-20 16:04:52 +0800

    passwords, passkeys, and passphrases

--- devops/python/Medium/pwgen.py
+++ devops/python/Medium/pwgen.py
@@ -5,3 +5,4 @@ total = string.ascii_letters + string.digits + string.punctuation
 length = 16
 password = "".join(random.sample(total, length))
 print(password)
+# Password Best Practices: We take a look at passwords, passkeys, and passphrases and offer recommendations for team-capable password management.
