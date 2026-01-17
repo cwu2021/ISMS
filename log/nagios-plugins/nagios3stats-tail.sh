d55346e5a899ad501f5aea8b0daaf8e58b8a0724
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-01-17 20:22:23 +0800

    [Checkmk Announce] New Checkmk stable release 2.3.0p42

--- log/nagios-plugins/nagios3stats-tail.sh
+++ log/nagios-plugins/nagios3stats-tail.sh
@@ -6,4 +6,5 @@ nagios3stats |tail -n +30 | head -22
  - A vulnerability in a plugin for the Windows agent could allow low-privileged users on the Windows host to escalate privileges to Local System.
  - Authenticated users in Checkmk could be able to break the configuration of the site, causing a denial-of-service.
  - Under certain unlikely circumstances, sensitive information entered by the user could be leaked via the URL.
+[Checkmk Announce] New Checkmk stable release 2.3.0p42
 '
