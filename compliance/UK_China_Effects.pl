712a07f9fc5b781c5ba538f6dc667cc8dec15340
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-01-22 13:00:56 +0800

    連環殺手正在剿滅連環殺手！FBI 怪胎小組出動，《懸案對策室》展開瘋狂對決

--- compliance/UK_China_Effects.pl
+++ compliance/UK_China_Effects.pl
@@ -6,3 +6,4 @@ Autumn in the Heavenly Kingdom (2013/1st~2024/14th)
 note: troops vs supplies
  - 1:2.5 during World Wars
  - 1:15 for modern military
+PAR「模式辨識小組」，集結數學怪咖、神射手、老派探員、新加入的菜鳥，再加上一個擁有天才頭腦、卻無法理解人心的男人——嘉德納．坎登，他們被稱為「怪胎小組」，更是局內的笑柄。然而，當案件進入瘋狂與無解的境地，FBI最後的希望，也總是他們。
