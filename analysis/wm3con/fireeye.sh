8e9909f5598d4623ed83feb965c8f06bf030ea4a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-01-23 13:06:49 +0800

    You can no longer use UptimeRobot to monitor for free on commercial websites

--- analysis/wm3con/fireeye.sh
+++ analysis/wm3con/fireeye.sh
@@ -7,3 +7,12 @@ unclutter -idle 0 &
 #chromium-browser --start-fullscreen https://www.fireeye.com/cyber-map/threat-map.html
 #chromium-browser --disable-features=Translate --start-fullscreen https://stats.uptimerobot.com/Y9EQoCDx0r
 chromium-browser --disable-features=Translate --start-fullscreen https://threatmap.checkpoint.com
+: '
+You can no longer use UptimeRobot to monitor for free on commercial websites
+UptimeRobot has updated its terms, and you can no longer use their free plan to monitor services for commercial projects.
+
+I think this poses a challenge for startups or small projects that are just beginning their SaaS or eCommerce businesses.
+
+This change significantly limits the functionality of the free tier, making it less suitable for businesses or anyone managing websites and services at a commercial scale. As a result, many users may need to explore alternative monitoring solutions that offer more flexibility for commercial use without such restrictions.
+https://www.reddit.com/r/SaaS/comments/1g15mvl/you_can_no_longer_use_uptimerobot_to_monitor_for/
+'
