d93398e8a6f01887fe94bff7fc7ad910e9ced87a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-01-29 11:05:09 +0800

    [Openvpn-announce] OpenVPN 2.7_rc6 released

--- workflow/CDN/vpn-internet.sh
+++ workflow/CDN/vpn-internet.sh
@@ -47,4 +47,5 @@ openvpn --remote 10.56.100.53 --comp-lzo --dev tun --auth-user-pass --ca ca.crt 
 [Openvpn-announce] OpenVPN 2.7_rc3 released
 [Openvpn-announce] OpenVPN 2.7_rc4 released
 [Openvpn-announce] OpenVPN 2.7_rc5 released
+[Openvpn-announce] OpenVPN 2.7_rc6 released
 '
