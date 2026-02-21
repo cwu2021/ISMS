055c3c82e9a8cf9ca27d6d7de2110f77db1abc13
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-02-21 10:48:05 +0800

    UniFi Introducing: UCG Industrial

--- workflow/WAN/fiber-backbone.pl
+++ workflow/WAN/fiber-backbone.pl
@@ -3,3 +3,4 @@ GBIC vs SFP
  - netgear has SPF+ for fiber connection between two switches
  - RJ45 for app, e.g. mds on port 3, ab on different vlan
 <=> unifi SPF+ connection
+ - Introducing: UCG Industrial
