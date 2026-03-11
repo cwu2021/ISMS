9f9c5b8d5bc18eefeb7045185611f7fbdc48f23c
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-03-11 15:33:22 +0800

    [Openvpn-announce] OpenVPN 3 Linux v27 released

--- workflow/CDN/vpn-internet.sh
+++ workflow/CDN/vpn-internet.sh
@@ -50,4 +50,5 @@ openvpn --remote 10.56.100.53 --comp-lzo --dev tun --auth-user-pass --ca ca.crt 
 [Openvpn-announce] OpenVPN 2.7_rc6 released
 [Openvpn-announce] OpenVPN 2.6.18/2.6.19 released
 [Openvpn-announce] OpenVPN 2.7.0 released
+[Openvpn-announce] OpenVPN 3 Linux v27 released
 '
