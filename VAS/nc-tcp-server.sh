73fe246f4d0d911385860e5e9f480338fb7c6351
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-04-09 23:50:11 +0800

    Get-NetTCPConnection and ss are efficient and powerful tools for monitoring network connections and  threats

--- VAS/nc-tcp-server.sh
+++ VAS/nc-tcp-server.sh
@@ -2,6 +2,7 @@
 # just for check compatibility
 nc -l <myip> -p <myport>
 # https://blog.gtwang.org/linux/linux-utility-netcat-examples/
-:'
-Trivy scanners for vulnerabilities, misconfigurations, and secrets with Ubuntu-centric guidance on performance tuning, security configurations, and scalability across Linux distributions.
+: '
+ - Trivy scanners for vulnerabilities, misconfigurations, and secrets with Ubuntu-centric guidance on performance tuning, security configurations, and scalability across Linux distributions.
+ - Get-NetTCPConnection and ss are efficient and powerful tools for monitoring network connections and identifying potential security threats on Windows and Linux.
 '
