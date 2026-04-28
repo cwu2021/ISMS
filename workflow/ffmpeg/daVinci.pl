745863adfe1a52998d4d441464b28655806c7801
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-04-28 19:39:53 +0800

    new 100G Ethernet based SMPTE-2110 IP solutions. Plus, we launched DaVinci Resolve 21 and Fairlight Live

---
+++ workflow/ffmpeg/daVinci.pl
@@ -0,0 +1,3 @@
+=pod
+Blackmagic: some new products we launched at NAB 2026 in Las Vegas.
+We launched a whole new range of cameras, switchers, routers, converters and much more. We also introduced new 100G Ethernet based SMPTE-2110 IP solutions. Plus, we launched DaVinci Resolve 21 and Fairlight Live.
