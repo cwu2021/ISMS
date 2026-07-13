cececb9ed9a506466ee90b5e11eb72ae08a48a65
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-07-13 10:54:06 +0800

    Security support for Bookworm handed over to the LTS team

--- patch/debian-update.pl
+++ patch/debian-update.pl
@@ -13,3 +13,4 @@ Debian 13.0 Officially Released https://www.linux-magazine.com/Online/News/Debia
  - Updated Debian 12: 12.14 released
  - Updated Debian 12: 12.15 released
  - Updated Debian 13: 13.6 released
+ - Security support for Bookworm handed over to the LTS team
