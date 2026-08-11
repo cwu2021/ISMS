326853ab307a62aaaef0c65c320099e9f799f7b3
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-08-11 19:22:29 +0800

    DebConf26: Over 270 attendees representing 35 countries from around the world came together for a combined 90 events

--- patch/debian-update.pl
+++ patch/debian-update.pl
@@ -7,8 +7,7 @@ available.
 Debian 13.0 Officially Released https://www.linux-magazine.com/Online/News/Debian-13.0-Officially-Released
  - Updated Debian 13: 13.1 released
  - On Saturday 19 July 2025, the annual Debian Developers and Contributors Conference [1] came to a close. https://debconf25.debconf.org/
- - On Saturday 25 July 2026, the annual Debian Developers and Contributors
-Conference [1] came to a close. https://debconf26.debconf.org/
+ - On Saturday 25 July 2026, the annual Debian Developers and Contributors Conference [1] came to a close. https://debconf26.debconf.org/
  - Over 443 attendees representing 50 countries from around the world came together for a combined 169 events (including some which took place during the DebCamp) including more than 50 Talks, 39 Short Talks, 5 Discussions, 59 Birds of a Feather sessions ("BoF" – informal meeting between developers and users), 10 workshops, and activities in support of furthering our distribution and free software, learning from our mentors and peers, building our community, and having a bit of fun.
  - Updated Debian 13: 13.4 released
  - Updated Debian 13: 13.5 released
