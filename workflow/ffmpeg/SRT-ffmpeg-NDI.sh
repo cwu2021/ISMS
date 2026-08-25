d003936e2b94cb12fdaf92e8d11a81c5cfd7d81b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-08-26 01:39:37 +0800

    Experience the European Debut of the Falkon X4 at IBC2026

--- workflow/ffmpeg/SRT-ffmpeg-NDI.sh
+++ workflow/ffmpeg/SRT-ffmpeg-NDI.sh
@@ -54,6 +54,7 @@ At IBC 2025, stand #10.A41, Vizrt will show how AI-enhanced virtual studios help
  - NAGRAVISION Launches NAGRA® Venturi,Intelligence-Led Streaming Security for the AI Era
  - Lionsgate Play Selects NAGRAVISION to Protect Streaming Service
  - Vizrt: Redefine Corporate Communications
+ - Experience the European Debut of the Falkon X4 at IBC2026
 '
 # https://www.haivision.com/blog/tag/play-pro/
 # HLS source may be found via Chrome -> DevTools->Network
