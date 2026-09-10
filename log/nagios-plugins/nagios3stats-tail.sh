d79d4de2c456dd5d1d4ca15463d04c77cf9de3a8
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-09-10 11:42:33 +0800

    [Checkmk Announce] New Checkmk stable release 2.5.0p13

--- log/nagios-plugins/nagios3stats-tail.sh
+++ log/nagios-plugins/nagios3stats-tail.sh
@@ -43,4 +43,5 @@ nagios3stats |tail -n +30 | head -22
 [Checkmk Announce] New Checkmk stable release 2.5.0p11
 [Checkmk Announce] New Checkmk stable release 2.4.0p36
 [Checkmk Announce] New Checkmk stable release 2.5.0p12
+[Checkmk Announce] New Checkmk stable release 2.5.0p13
 '
