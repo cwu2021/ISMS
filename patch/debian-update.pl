d55c7e27c566c5059260724af385ffa18665ff5d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2026-09-13 13:01:03 +0800

    Updated Debian 13: 13.7 released

--- patch/debian-update.pl
+++ patch/debian-update.pl
@@ -16,3 +16,4 @@ Debian 13.0 Officially Released https://www.linux-magazine.com/Online/News/Debia
  - Updated Debian 13: 13.6 released
  - Security support for Bookworm handed over to the LTS team
  - Debian 11 Long Term Support reaches end-of-life
+ - Updated Debian 13: 13.7 released
